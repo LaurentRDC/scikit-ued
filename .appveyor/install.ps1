263abfb564a825b94f95402359f0009b96bf4c5a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-22 16:25:26 -0400

    Installation of skimage through conda

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7f6844fb8345dd915965fafa9bae406658c3645ece09b209d5164f29e1e2879
-size 6737
+oid sha256:33860e0adc7ae2ccf5db17a4137e49d151e212e99f61c5e77f1ac2afc8d61cb2
+size 6750
