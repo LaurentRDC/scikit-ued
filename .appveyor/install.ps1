36f124ece3aa8b73fdc0aa9ccc9eeae23cb42503
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-22 15:59:13 -0400

    FIX: Install numpy & friends from conda on appveyor CI

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cde31cce48819c6389c11f6ee1501756393239fe51171212ed4388440ae7c06b
-size 7194
+oid sha256:f7f6844fb8345dd915965fafa9bae406658c3645ece09b209d5164f29e1e2879
+size 6737
