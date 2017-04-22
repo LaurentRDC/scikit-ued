827de62c0e78d7d2c3f41d7e1998996e9a0ae144
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-22 18:41:02 -0400

    Refactoring of the baseline package Iterative baseline functions are pretty similar, and they now share a common implementation for the 1D case.

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ba7df0444a03811f27b8e98ec01a6c8d001c8b932192c463e39dd228eac79db
-size 12250
+oid sha256:645b6cf155c0b5a24b32175b5241c56a32166ab7ea7452d69458eadb9aa857cd
+size 10492
