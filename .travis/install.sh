7c7d218bfdf8fb654186be8eb45e00fb6c2e56e9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-22 13:00:56 -0400

    FIX: travic-ci install cython

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84ac46bc5eebd1c75e1498539d9538d2eb3acd84e2adf9860178582de8e3fa45
-size 996
+oid sha256:7040324434c6163b635ca8d222055ce808585e6441499ed6c67d4c45f6ebe9c9
+size 1046
