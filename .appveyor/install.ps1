5be2cb6992a8077f5663e6bb3ea734328cf70a15
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-24 09:03:28 -0400

    FIX: Python 3.5 install from Miniconda3s Former-commit-id: a8f7ab922738cd320fd386bf374a96a1fc7dcb95

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33860e0adc7ae2ccf5db17a4137e49d151e212e99f61c5e77f1ac2afc8d61cb2
-size 6750
+oid sha256:0b5c5182b10f5e07cea4869f69e7e6f47d14ec0a68f18e2ba6d7d9ed11b97619
+size 6658
