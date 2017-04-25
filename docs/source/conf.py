80844f16c86f671f0e7e6c8768e27358edb3421b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-25 08:40:25 -0400

    Plot utils: spectrum (rainbow) colors Former-commit-id: 3927f142dd22833c231f32730778094e1239d3a4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f442bcde375f3a4d2d6f29ee214b73862dd96665656cd6ec568085536af26669
-size 4180
+oid sha256:51c90c5a7b646c33626cb68624bc530fc71f38e52d438b090b08c5286ba671fb
+size 4256
