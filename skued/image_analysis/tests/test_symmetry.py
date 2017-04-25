98ec9b4a28876e2b02880892df3498a62b904d18
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-25 06:51:49 -0400

    API: angular_average returns intensity and radius Former-commit-id: 9db4a4919b5bbc8447e5e588254b0aec0b485d16

--- skued/image_analysis/tests/test_symmetry.py
+++ skued/image_analysis/tests/test_symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82eac4c01992d12ff3de3ddc7dc9cad0127648170b22f2b9b8eb352fa2a370a
-size 1143
+oid sha256:6d8bca5d6b3ae1d9e98b5bde92c5d49168b8241b56348ad12c336897d4ae2867
+size 1135
