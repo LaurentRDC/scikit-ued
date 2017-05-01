93aa58fabdab00eafe82933fb118304ee6b12810
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-01 17:18:29 -0400

    ENH: powder center finding in noisy data The correlation of image and its mirror is post-processed for better peak-finding. This is not optimal yet, but better than before Former-commit-id: 6dcb860c5c1bacc398344ce595c01b6ef6eb173e

--- skued/image_analysis/tests/test_powder.py
+++ skued/image_analysis/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc5df458aea7d197fbffdc97437df02c009e989073794bfd9fbca07926650756
-size 2707
+oid sha256:0252cdf37d39783890d5a61a188a113ab2ce77ce8711045437965c8f90d0dd36
+size 2658
