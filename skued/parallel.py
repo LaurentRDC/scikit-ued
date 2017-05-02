094e819cccacccad663e7adce855476ee09ef7d4
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-02 18:41:05 -0400

    DOC: fixed some documentation Former-commit-id: b81e3e3f0e27f4679bc53a82e745589484add8ed [formerly e3c550194e75623461dbea3fe9f118205567b35e] Former-commit-id: 61160adeac2b8e5529379be813fccff379cd8569

--- skued/parallel.py
+++ skued/parallel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37577243caeb6256b754d5d61d0dc2cbc101f628b3ace59aa49991c6fd00cbfe
-size 1773
+oid sha256:bf406014c3c37068c065d5e31c58867bd94079fe51d4d5566122d11bab956275
+size 1601
