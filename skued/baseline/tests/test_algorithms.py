232c83a625a3c2ccba81d5a792757b6b7e3aa42e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-03 14:58:32 -0400

    FIX: odd-length signals alogn axis for baseline functions Arrays with odd length along an axis would be mangled by the numpy.resize function. Added better resizing using numpy.swapaxes Former-commit-id: af179389ecb2c2be7674db80f901f697f5a508e9 [formerly 03a61d68360d0f69b69d8e318916a1bed0cf6d0c] Former-commit-id: 9777cb1c19b92a0fd16c5882f8c861066a3016e8

--- skued/baseline/tests/test_algorithms.py
+++ skued/baseline/tests/test_algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff40d3871d983377e5a50e8ae47030029b6e47d24d68df616a3fa112c821003a
-size 1203
+oid sha256:f27b426d1e41d196dbea377085be6c196d00da6ebbf208210e2015739646a1e2
+size 3290
