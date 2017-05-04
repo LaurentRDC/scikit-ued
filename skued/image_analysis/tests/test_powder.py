580d790774f2b5a1214170de2aca7619e5a9a129
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-04 16:13:30 -0400

    Added preduce function Parallel reduce function Former-commit-id: da3c64553fe133318de8ae1c9b1343c6c8a0a05a

--- skued/image_analysis/tests/test_powder.py
+++ skued/image_analysis/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0252cdf37d39783890d5a61a188a113ab2ce77ce8711045437965c8f90d0dd36
-size 2658
+oid sha256:bec8c3fad0b622a14323c431aa942e0f2395be958073eb28399ba984ed40a09f
+size 2635
