60a9a39461333bafc356317e11b9c9b63cff8dac
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-04 16:13:30 -0400

    Added preduce function Parallel reduce function Former-commit-id: 71bf521717964a7994c202a46ea7661f5bfb67a1 [formerly da3c64553fe133318de8ae1c9b1343c6c8a0a05a] Former-commit-id: 2674cfda4204d18194eb722e9bd665f3212dd9d4

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:395e31296a18a03663f3e1d7885c372002ff99283e1ccef560d4fbb696a1fcd2
-size 504
+oid sha256:3f9c7081b318233f4ea2c67e7b8a765db129dadec56c3e992d43ad84fbc6497e
+size 513
