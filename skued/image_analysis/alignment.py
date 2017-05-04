b9e47f62122a6c365fe2dea3ab15312ae90181a2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-04 16:13:30 -0400

    Added preduce function Parallel reduce function

--- skued/image_analysis/alignment.py
+++ skued/image_analysis/alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27635b70b1a394be7a825950326d2430510e729dffda8b3168c327359baf71d2
-size 1996
+oid sha256:9504042e620c07fe77e0a02c2c88c19b6acc0bd3064fbf743be4d99700d7c24d
+size 2102
