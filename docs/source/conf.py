454feed55ee816f3047637e0538d02fccec8c7fe
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-04 09:36:18 -0400

    simulation package skued.simulation package has been created with the powder diffraction routine powdersim Former-commit-id: 652884c214d0d0645f7b88e910edb1d7efe91226 [formerly b52e108576f5ca86d25c62009e44cd197e124a0f] Former-commit-id: dd86a258a52e96eb7c633e0305b694d2e5056bf3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eb89efc54c6aeb56c876533d9e7c3bb2a5b73da8cdd6e0ed95ed008ea073d2d
-size 4257
+oid sha256:cae099923a28d680a1ca6ef4abf05551b127b2589200da638e232fb29348c29b
+size 4284
---
+++ skued/simulation/tests/test_powdersim.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9e34d8d5d77f377f000ec7bc31c67d0ee647c4514d32f8738326586c8ae40b2
+size 545
