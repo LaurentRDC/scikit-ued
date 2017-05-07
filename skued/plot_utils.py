8edcea6b4510b05caf3440112479a34d70190d1b
LaurentRDC <laurent.decotret@outlook.com>
2017-05-06 20:04:50 -0400

    FIX: plot_utils documentation Former-commit-id: fddc64103442f053d81efcdfd53b94721c6bdbeb

--- skued/plot_utils.py
+++ skued/plot_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:132a9f1d42a08b3ca27933d27d6d4471f48bb45dccb9dd050d58780eb58e1783
-size 1244
+oid sha256:dc1ba5aa0ab4efd09d50c0cdf98a4f929032272b38ecfba9fc1855021e1db468
+size 1224
