a2d5b30a45388e3cd2ec05d418acab3491f08d0f
LaurentRDC <laurent.decotret@outlook.com>
2017-05-08 14:58:46 -0400

    Added quantities module for physical quantities The quantities module helps calculating electron properties based on electron energy Former-commit-id: ed8ed0048efd68fc072dedfdd6e1fabf1300aa7e [formerly a439db30f9ceeaf429d36a50ae1da92b5876c45b] Former-commit-id: 5de98b2c259c5a273edaf926ade0448c24263dc3

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f9c7081b318233f4ea2c67e7b8a765db129dadec56c3e992d43ad84fbc6497e
-size 513
+oid sha256:c56c6073f3065cb7778b836b6c65ff10db80755d95fc6e7422f0a2a18bb7a013
+size 589
