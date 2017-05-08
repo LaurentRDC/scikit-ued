95e72cdc30e1fc2014e28a1cbcc47fca9f7e079c
LaurentRDC <laurent.decotret@outlook.com>
2017-05-08 13:43:27 -0400

    Tranformable is now an abstract base class Former-commit-id: 498d5d8182bbcefb90f6eac7b1253bd7294f7cde [formerly 7da75d8b965a87d2594903636bd3395b77382208] Former-commit-id: 2d5efa747a501e39175cb7e4ac6d6528b02526dd

--- skued/structure/transformable.py
+++ skued/structure/transformable.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26933c366afa4731da313a8cfc1d55cdb199c2eb6d255e47ddfc72ad29252653
-size 576
+oid sha256:cf5167f55886a772b5d565ac1b4537ddebce96089ce188917b021f10f3247e3b
+size 780
