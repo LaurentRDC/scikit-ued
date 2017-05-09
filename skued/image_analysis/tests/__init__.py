d0d63b28ae5aa392274346017fe2ecf9e3f7a85a
LaurentRDC <laurent.decotret@outlook.com>
2017-05-08 20:24:34 -0400

    Encoding Former-commit-id: 6ed9f07a550ef67958b41e392ef158cdfc5da887 [formerly 4074e8448bf3bd6e57fcdd9c27222eef1f06fd29] Former-commit-id: 6152a95906476c677d766255a03fc81a786b125e

---
+++ skued/image_analysis/tests/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e45434b3fdab3dad95a63fdd3a1a7abac5d70073d3f57f8c0555592cfd94a2aa
+size 23
--- skued/parallel.py
+++ skued/parallel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ac250d946b4d0ca21e10f7e7d73c724250c89eae604ccfb016f0905c1ada960
-size 3240
+oid sha256:0a21d499e063637d455ffb6ef60d49dbde7f0b95fb8c0f474c0c2207ff1cc0f1
+size 3252
