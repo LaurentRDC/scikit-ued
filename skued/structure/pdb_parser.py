29c41cfcd44e0be17417b3e2b481e26ba9fc27a1
LaurentRDC <laurent.decotret@outlook.com>
2017-05-08 20:23:38 -0400

    Encoding on .py files Former-commit-id: 99e191f1c25d7fc2768f39d40bf98724cea92096

--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f38103121e1ce761a97d452c9bb2e41e432b34e84f7f06e10f0aaf68b8cdeb0
-size 5689
+oid sha256:df8eaa03c5e8494fe5e8143ea797b646cd26949d0af6bc7d43f677eaa5fbd5be
+size 5713
--- skued/transformations.py
+++ skued/transformations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c983b879edd4eaacaf79aeb38e21f9ba7d55d3efe13056f79f2531d4c0adf9b2
-size 7844
+oid sha256:bc82fca76ab9f3cf218599df18180511d21366c6ac3758bf870ec780a23472ef
+size 7868
