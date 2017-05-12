45b7e7ac1a75ed6d0cdcb63aec72c9e18c42750c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-12 15:24:41 -0400

    2D baseline_dwt Former-commit-id: 741c158223737c875b0731e76ebcfb5640af5ae7 [formerly 8a65614f2a5ab700b4553e3e0d1b4ce2b88fd85d] [formerly d8433395b63cf2b5b55b89244455e52817778cbc [formerly 67dfed2e17d588f83b4ac1b2debe818aedc3c1e0]] Former-commit-id: 0fc6dd0375368c5203e5b2600fd5e1d28824c8db [formerly e778187e31a76d0467dafcfee75e26158d5b0a19] Former-commit-id: 98424455c381df39f3d86b0978c7efefc469586e

--- skued/baseline/tests/test_algorithms.py
+++ skued/baseline/tests/test_algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f27b426d1e41d196dbea377085be6c196d00da6ebbf208210e2015739646a1e2
-size 3290
+oid sha256:6004e325d89d77c0449bc2bbe9b855dab64015023b4c390b426c33d3f659e289
+size 3982
