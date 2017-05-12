9516a60d77933224fbd17bd9fafcb96f194c49c6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-12 15:24:41 -0400

    2D baseline_dwt

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bdf2b90f01e64400bc1b40523c1dad85ac50cd7744600cf8842e250c96d9baa
-size 10681
+oid sha256:e96148df9f58a8c8ecdf85717cbd93de171cfa8ef57e90f7147f8a40c7277355
+size 15050
---
+++ skued/baseline/dtcwt.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9d5da5032f628541db3a808fc3ac2b2a2b0995392541c903ec4c09534a7dd17
+size 12060
