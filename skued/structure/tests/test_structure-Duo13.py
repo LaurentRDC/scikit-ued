b6e2dd9db90b84a2d02ad2b1ce7046af0d030f1c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-12 10:44:58 -0400

    Code cleanup Former-commit-id: cf71dcc232b7a54e32243d5017688400c528e638 [formerly c89b08e1789f5f2dd5d3564f1eb461a6cb068c3c] Former-commit-id: 5ba2909f002e9e564cc17d06f3b64e1d6fdd649a

---
+++ skued/structure/tests/test_structure-Duo13.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ebb08282b01f7bb6f74393f35604d5a2f106c30259b8ae095938b022d500e574
+size 1219
---
+++ skued/tests/test_quantities.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c5f8fea18a8601efc09a60b571fdfa27fc70f90a3a66ad51642f311cabd36cdf
+size 1340
