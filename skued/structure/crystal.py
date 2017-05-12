edd206288f4c595a7ce33c6bfb0955c82a9052f5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-12 10:44:58 -0400

    Code cleanup Former-commit-id: c89b08e1789f5f2dd5d3564f1eb461a6cb068c3c

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63a8fd55d5b38cad33d033e27707bd07e13281d833f9c3d79b9e63b7928f6cc1
-size 13972
+oid sha256:9aeeda721e3ddc9ab245642aaa32690abe3ab7b48443a82b3f7195f9357e90b0
+size 13981
