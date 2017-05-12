cdcc3a3f4155940d389f3d925159a0ca88900f40
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-12 15:24:41 -0400

    2D baseline_dwt Former-commit-id: 67dfed2e17d588f83b4ac1b2debe818aedc3c1e0

--- skued/baseline/tests/test_dualtree.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9ba9fcd0d800a53f52b0540f6830884c28ab2d90061601daa9f701eb7a6901e9
-size 3351
--- skued/baseline/wavelets.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:66af93e917f6d5dbb0b3f603a01052c40ea77f152983376cadff56809600750a
-size 13627
