9fe9172640259713853504b8e182f60060dc21e9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-25 17:20:56 -0400

    FIX: CIF Parser cif_parser module was not correctly interpreting CIF files due to multiple possible values of space groups. Next step is a better parser module Former-commit-id: 5040446cb49b3ec4fa53377b1b600c2797f704e4

--- skued/image_analysis/powder.py
+++ skued/image_analysis/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:721a5c4b35b4acaa8124096abcad883a4aafe1c3dbd8d2245754e61919b8882a
-size 3751
+oid sha256:96dcd20764057e862437160962129b2f279afa4c47677a074d608a58c4e44aec
+size 3755
--- skued/structure/tests/test_atom-Duo13.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7fd9b4892cdb431233be48023ea4826aaec1baaecad60d880e693b78efdc294f
-size 198
