d9ce30a222674529d529215976193efc3013a64e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-25 17:20:56 -0400

    FIX: CIF Parser cif_parser module was not correctly interpreting CIF files due to multiple possible values of space groups. Next step is a better parser module Former-commit-id: 5040446cb49b3ec4fa53377b1b600c2797f704e4

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e96148df9f58a8c8ecdf85717cbd93de171cfa8ef57e90f7147f8a40c7277355
-size 15050
+oid sha256:3d4437014f9e2369e52aeb85cdee7c2fef45ed4563bb63e744477a98fbee5e16
+size 13390
