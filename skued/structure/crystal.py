d836a12d12e1ecc9d9fb0ceb1882912cc2584552
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-26 17:44:11 -0400

    FIX: doc references Numbered references are autogenerated with .. [#] Former-commit-id: 4564d76c1c68f5a1882cc0eafb473ead6c1dd6ce [formerly 860aaf092d7cbb607a632be236eb9bdf279e96fc] Former-commit-id: 27d4bac6397a4b7c265e2b43dbda02a20df65c5f

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc38a05ca37b339fbeaeafe4cb481bf4d90b79823108d004e5f0bb3be61f4d9f
-size 12209
+oid sha256:161ebfa42736501c303f9c8d981a09b6a59c5fc86513939b46e1ae9f96edbe05
+size 12428
