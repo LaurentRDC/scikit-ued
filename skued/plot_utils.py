31d1889be4dbb0b370b727e606ea025ab2fe1291
LaurentRDC <laurent.decotret@outlook.com>
2017-05-27 17:29:10 -0400

    FIX: parallel module chunked() re-added Former-commit-id: 0712372d81c1ee0b4ce4e97fe55f53913bd77259

--- skued/plot_utils.py
+++ skued/plot_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e3d84918809389c74d12bfde593fc31fac1e6a4a319d7cb660f5af97e13b44
-size 1294
+oid sha256:1089bd9f102b0e0731e444a9eafb8c309d2f3a947728fb2560d2582adcd4bef9
+size 1108
