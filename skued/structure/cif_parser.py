274b12fea90bfbe6e29aa1c427c50a3d78c05dca
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-29 17:56:59 -0400

    More robust CIF symmetry operator parsing The CIFParser will disregard Hall symbol in file if it is not available in the internal database, and infer it using other information Former-commit-id: e8f47c087b628943284d110bcba0525c8b5cf52c

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:794cc59c27c6c32edee577f1581e7daf073ffda965450a3953347df4b81e37ac
-size 7518
+oid sha256:3bbc2071c4bc1cbff869acd7477f95c4bc2be1f00457a128087491b317d5f4b3
+size 8093
