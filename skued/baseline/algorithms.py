fac3d6ea6b9a006508751d71d921f8057cb8ec27
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-29 15:09:06 -0400

    DOC: baseline maximum level is None, not 'max' Former-commit-id: cd467951b6630b5fdac44188e0204d82fa20d8d4

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62736b3f95c8a593243fbfcbf98665362586a35b79762e5efe47d3c02658b231
-size 13412
+oid sha256:1c226e5cf3368ff01fc798efd5fe123b4b7b35012d9fb6c44d5e30cc4b75cb09
+size 13410
