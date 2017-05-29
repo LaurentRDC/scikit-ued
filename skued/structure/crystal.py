808a962cc5d11a33530b93b5648b50c9b5793ec3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-29 12:07:21 -0400

    Renamed Lattice.parameters -> Lattice.lattice_parameters This is to make it clearer when interfacing with the Crystal class: Crystal.lattice_parameters() is more telling than Crystal.parameters(). Former-commit-id: 95247a214a8d433e1b9a0cb2c1b575ca4ab3228d

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35280845b97ce764ecb79d8a8c090d2242d62e46ae0bfedbac14f7c47180abc2
-size 11303
+oid sha256:ad24cab8d50e96dd62e60ddbe51605a7ec0c6049993ce2b937d40fc1a721b5b6
+size 11357
