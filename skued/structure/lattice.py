3a2be043070c10711286ba6e03c65a13a1b1684f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-29 11:53:19 -0400

    DOC: lattice_vectors_from_parameters() Former-commit-id: 16b927713d48a2b42ec5675c787b86960a01dac1 [formerly 943585ee566d6bda3aa6aec758823f3a8998ebba] [formerly 1aeff2ab5852bd5568799490ff86c77442df969c [formerly ce369fa23fbbc864e29cdcf7d8169fa2ee8d4dd5]] Former-commit-id: f86b3629cded790c131ac2418d3d7a177c428076 [formerly 9b50277fc6da493480dae7972c5943fadd41c874] Former-commit-id: 5de63d921ab086dd67a3aaefb1172adfca84b5a9

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d1a886407ff1fcee87b5fb12753c16fdace43a2e67b0c3c8bdb0581fd50f71d
-size 3872
+oid sha256:0524a718ee30756f931bde68ddff93e02b277fdb84efaba6ebeb6100b70c7980
+size 4190
