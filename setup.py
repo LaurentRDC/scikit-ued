c8935b4a9bd5082659b297aa77232f6a2e3bb0dd
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 16:12:39 -0400

    Code cleanup : updated setup.py and cifs files moves Former-commit-id: 91c0660b8dba92030d0a1f3ff4c433c6a89e3be6

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3942e3386552b92dd1ca214c99e96b589fd9e9fc7b39e32c1309f703e4d8eaa4
-size 2162
+oid sha256:8279ce4417c980631b0cb00f3bfa25cbafd4d9736c60ca6fafa17b82bc6e38b1
+size 2174
