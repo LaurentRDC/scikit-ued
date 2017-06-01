743e546b3dc111bfcd78ea5be9c54cf7c55b3199
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 09:54:38 -0400

    Added subclass check in Atom.__eq__ Former-commit-id: 049ce79bc17699cbb1674108f5b5cd0838ec0ecf

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac3ec027a800ef96d4cbfbf1928d5197062956cf5257f9d52ef513a9cf6b852d
-size 9637
+oid sha256:78c5c909ac8692e9d76742c4b717b5311e1d360a1d57394a02bb8a0a271ae116
+size 9770
