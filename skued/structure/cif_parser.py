3c3a87e764096969d75a0215d7bbc7867780a96c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 13:01:37 -0400

    Version 0.4.1 Multiple changes regarding the structure module; * made Atom objects hashable * This enables the Crystal object to be transformed in to a set() object * Refined the handling of symmetry operators in the Crystal class and CIFParser class. This will reduce the number of atoms to a primitive unit cell, although some work is required to fine-tune this. * Updated tests on Crystal objects to make use of the new 'setability' Former-commit-id: 78dda2768ca2f53321a161c408dd650010aed9c8 [formerly 0cfacd08ef5f3d070c6145656a73ca22f60038fa] Former-commit-id: f1f79680a4ac22029448e905738bf19f9d052096

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bbc2071c4bc1cbff869acd7477f95c4bc2be1f00457a128087491b317d5f4b3
-size 8093
+oid sha256:370f9d53e1d1d16520e2bc46a653634eca330721124526f2b2462ab6979e12f7
+size 8264
