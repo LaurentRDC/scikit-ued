ac9bb7d81474e886110f3145f5e1ba161ffd681b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 16:12:39 -0400

    Code cleanup : updated setup.py and cifs files moves Former-commit-id: 91c0660b8dba92030d0a1f3ff4c433c6a89e3be6

--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df8eaa03c5e8494fe5e8143ea797b646cd26949d0af6bc7d43f677eaa5fbd5be
-size 5713
+oid sha256:42018f11dba58f6ea9f01b8f9ede42b3c198f40a4157b21f1b4462d7f825d2eb
+size 5139
