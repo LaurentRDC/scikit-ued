294b155ae7c59c10200a2848f334cffb971891a2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 09:52:25 -0400

    FIX: typo

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:569270d99babbfa2e34bb0e2b38073b33a013c39d0744193d817cbfa88c90eb3
-size 9639
+oid sha256:ac3ec027a800ef96d4cbfbf1928d5197062956cf5257f9d52ef513a9cf6b852d
+size 9637
