79f03dab90cc797cc28b317560d0ced6e398ea92
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-01 16:12:39 -0400

    Code cleanup : updated setup.py and cifs files moves Former-commit-id: 91c0660b8dba92030d0a1f3ff4c433c6a89e3be6

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:370f9d53e1d1d16520e2bc46a653634eca330721124526f2b2462ab6979e12f7
-size 8264
+oid sha256:dd28a920cdeb8128a3b8d38ab2cb2b27d5dffd28a2797be08379b34b182adb1b
+size 8262
