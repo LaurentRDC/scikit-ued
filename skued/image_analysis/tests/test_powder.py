a40e9e6aaec6e64b9b79e627c91eba0f40ce2c80
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-05 13:30:33 -0400

    Added the possibility to compute an angular average over a certain angle range Former-commit-id: 3a9c5e1ee8bdcc39ec3f2adb8143e88017777396

--- skued/image_analysis/tests/test_powder.py
+++ skued/image_analysis/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e450a67e767f6e49fe7d94e4109194497e034dc58e7550f60aceb78162b460ae
-size 3393
+oid sha256:c3b9c6acdb71dd6aa317a2d8649d04700a5dd0dde465b9817acff5240092b026
+size 4286
