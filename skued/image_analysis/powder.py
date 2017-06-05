6c074929f09d2008c6af300a5531030ad4e6fcd0
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-05 13:44:24 -0400

    FIX: added supported for angle bounds outside of [0, 360) in angular_average Former-commit-id: 63f9222e40e2b58d3ac53b27085f8e3a795d3510 [formerly 4fd7496557b64cea51e3609d9210e89a7c654da0] [formerly 8011da72ae7ff173f4e0ae4f296a12054a0b2565 [formerly 10846bdebb4baf0f469ab817bd732d536b7b0208]] Former-commit-id: 8aaa9b989248e9c36a173709f5def18365abb1b5 [formerly eb367022b2997ac767d9eb6011fe3371d5254dc3] Former-commit-id: 4698d8874b0fb972dee81cfb557c78c882747ff6

--- skued/image_analysis/powder.py
+++ skued/image_analysis/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c5ddae4b3ad08872cc2a4d999201766436607575e997b8fd44251bfcbe0d053
-size 4105
+oid sha256:35d7cf938515c41e27aab6ff5e18be674f4027e4c6c5f376c6eee54e0df1f37c
+size 4243
