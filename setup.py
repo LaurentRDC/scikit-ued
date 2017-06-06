2733bf8aa47c4c71c3bf2552c9af174300af0b73
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-06 09:42:56 -0400

    FIX: wavelet filters not being installed with the package Former-commit-id: 09b15b0cc8d31653b8f4f99b3e15b50f9065d22f [formerly bd28ec6567fc52a30db7ed5cd2fef00bfc1ac802] Former-commit-id: a3c901ee6004eecf82e147d45f0c74be089c6ea5

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8279ce4417c980631b0cb00f3bfa25cbafd4d9736c60ca6fafa17b82bc6e38b1
-size 2174
+oid sha256:65016c5ab482671e7a7cd39a7a0e94c47dc0b28912433a675f440d4d7dad4af9
+size 2427
