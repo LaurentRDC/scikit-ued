847a55fcfd287ac5c3f8832867b187905bfc31d2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-07 11:17:52 -0400

    Added nfold_symmetry image for averaging images with discrete rotational symmetry Former-commit-id: 0c4617de0051d45d053b92e0b79d9ae46e73606c

--- skued/image_analysis/powder.py
+++ skued/image_analysis/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdcfc0dfbe3dba6c6fd95c7278e12477a346b9c9a267bb97874aa92cef78121f
-size 4141
+oid sha256:2eca73918e2a836c5011c133362a82376df30550ecaf4b202f5d55c4450b6c6b
+size 4053
