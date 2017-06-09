34697ce34411c44848d678a621b63d708c972d81
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-09 14:18:12 -0400

    Removed Code Coverage integration Former-commit-id: 3c4076b0928212f6de2510e23714ff8629cddbab

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c63660d0a1bc50e4d2f1cd82542de257aa81d593fe069b75c10ae2902d57259
-size 2441
+oid sha256:83d8dccf5dfe8890b1a05437461ed1aea392f7cc8adb424f1e521deca4c7bd96
+size 2510
