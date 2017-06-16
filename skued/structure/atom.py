c7d9a34c48f63037fc50eac28ba5cb31295b6313
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-16 16:41:22 -0400

    Update to CIF Parser CIFParser is at least correct on graphite. Further tweaks might be required.

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d059fe643cc6916c21e5300c7b475cde87d7245fa3b806817559236dae223c4
-size 9809
+oid sha256:5c217d8a993626a6128a4637a4d830c8012a58d8bb38f43c7958daee6bd28e63
+size 9841
