9bc845b16bdd3e620d4886b9795cc9adea02c8ab
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-16 16:58:46 -0400

    Code clean-up Former-commit-id: 74af92a99662321a57da21173397d8bab03965b6

--- skued/structure/tests/test_cif_parser.py
+++ skued/structure/tests/test_cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2d3f982e122a9abea922a6b36c227552b88d0f034848496b52cac283519caf8
-size 2964
+oid sha256:1635fc1a3ae174fa6bb7240af102304f5a308debf42d2a1abfeef73fdac5a60c
+size 2927
