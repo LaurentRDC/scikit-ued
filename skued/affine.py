10719bce2f4d5266130c3b26231669ebf77b8f98
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-16 16:41:22 -0400

    Update to CIF Parser CIFParser is at least correct on graphite. Further tweaks might be required. Former-commit-id: 98e2daa21eb8e3b1e072e7548badfce40b42c264

---
+++ skued/affine.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c5755d6662dc53ea87067e1129f948af0f13d011eefb39f9725a5d4e7db12bc
+size 8493
--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd28a920cdeb8128a3b8d38ab2cb2b27d5dffd28a2797be08379b34b182adb1b
-size 8262
+oid sha256:066b05bc26868ce5241d9dc0272a5c6025649ed2c4286a69515459bd299116f7
+size 8421
