cc498e04c31426d82142f0d27c09ffc45cc1176c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-16 17:01:43 -0400

    FIX: importsd

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5200ab6950f6f0e7ece363c5b2f1929ef3dbc8890496f7b8ed698078de563e8
-size 8334
+oid sha256:4ae2bba2d71e598897e60ad0da42f802fd5c46a2633b2c69ece45922195de0de
+size 8375
