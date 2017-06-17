545d3cd0b83709d496b2d0194221c5a5aa76941e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-17 12:58:45 -0400

    Added tests for CIFParser on monoclinic M1 VO2 Former-commit-id: 6a5b398f4e019c6a4cab7864bac7586affe6167d [formerly b88b02a42c761ca4faa96dbaa050ffa29d9b4dac] [formerly 48ecc58094d26755d707395c4781a13ab7fe5cc5 [formerly 4137c44dded5e62e807c058b104ff8bc8103a15b]] Former-commit-id: d603a2672d33de515e1e55545474dd4c83d0a17d [formerly d97337dbb003dd6295cfdccfac091985c81507d5] Former-commit-id: 3ee836deeb6cc825b04ad55cdb49ed11f047f269

--- skued/structure/tests/test_cif_parser.py
+++ skued/structure/tests/test_cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c87166b7a479358ff9e5edebe9c7fdf4a815541ccc2cfb308b512c2623e2fde
-size 2038
+oid sha256:0c8cef6143ea22fa3b94d91456c4b0f4e7e36a95319ffb23e9b43ab03cef3525
+size 2526
