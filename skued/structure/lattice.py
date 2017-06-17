66815ec807e4167b9df54fd85a2f5e65b12586b9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-17 14:11:59 -0400

    Updated documentation Former-commit-id: f5a86a7ff5ca4c9a59bce055df37395acab84554

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3bb33a58016d1fb99b11eed5eb06c78494b51fc8cdf935075503b2e4c588008
-size 4486
+oid sha256:146a8c444f1cb82ff7980efec5b6e766aac25a1daba311d1b3e9e16231a71e4d
+size 4212
