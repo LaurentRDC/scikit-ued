ed999d2e3a0160348f4cb7b50b856b08813b7411
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-18 16:09:29 -0400

    FIX: forgot to close file in CIFParser.__exit__ Former-commit-id: 9b64954985daae785802bc7a2c357170f1e80902 [formerly b3bace4bd70c30e300cc947a0661981215bc3a4d] Former-commit-id: 5ecfeaab86333531de84f72fd5c9a2da7e3c1886

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d42d6d2f431134c35a28006441296d6249b4139ceb9f62322c1508f6a8da4c73
-size 8692
+oid sha256:43cc80dfca8ca11343a9869dd0d9b6ead72f9eec012ea6bd0b4a26175c3464fc
+size 8726
