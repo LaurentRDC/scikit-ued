3a02c915341729badd5cd146a9da8e3e1ce71006
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-18 17:04:21 -0400

    FIX: Atom.electron_form_factor was modifying arrays in place Former-commit-id: 636a014d45ee562a166bcad2c236e81f2fc2c4b9 [formerly c3aac0405a6d23606d8cc8e4737e96bec79af3fe] Former-commit-id: 2c8f57d8bad1879537c0b274617d3d1e11455ef3

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b4109568dc065625dcf8ce3954201aa6485492dc6e3c4692d64b70a8177f741
-size 9841
+oid sha256:fc9beff271bf54cd970aa3ce5989ecba14af089c0c6434395a26515899a6ed5e
+size 9965
