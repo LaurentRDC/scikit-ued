050de726baa2c6fa533767bf7ea7ee4e61bcb7f5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-19 08:26:16 -0400

    Added more tests for quantities.py Former-commit-id: 952ea0e72efb5570419a6fd353c7eca35a61be73

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f333a440927a051557cad9d58e8825a0080a1187b22f1a720015da675547b20
-size 10094
+oid sha256:5997783c6543a1b9d949ef450fecfe3c7971f1afce76adb8e3b376d7b86d23c7
+size 10059
