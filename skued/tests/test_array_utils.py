ca644663b0834977b76825abd075d0c5e161e2c2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-20 16:46:43 -0400

    Added mirror() function for reversing arrays in general N dimensions Former-commit-id: e0eb2125413b857fd3d544c983cf596ef304c1f1 [formerly ca519f191f19cfd8a136df837a58ec1225a82c41] Former-commit-id: f957900dddd5911d47e0938762dfac350503a3ed

--- skued/tests/test_array_utils.py
+++ skued/tests/test_array_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8842cca922324c3a7fac68c48949c3c64c0a7c445eea012661acd7adf959ba9f
-size 1274
+oid sha256:a12251b9dc4c1e8cc7bb3d102ec62a772518c13d976b63d499e4f3732f828a9d
+size 1983
