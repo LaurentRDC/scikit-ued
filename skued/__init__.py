4780deedc0cd9358a2dcba1b6618a594b7601d0c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-21 17:27:36 -0400

    FIX: readme Former-commit-id: 3f3dea4bd7b0ff71a5e72e2db3bad0247eae3155

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9668fd235a9888da5eab6674298876231d405d1b966a7a9eef817b7a5c248c3
-size 707
+oid sha256:0f67fc6a1978601fef6576459833b9a91c236e1338c3726fec9914cf1d9c1ff1
+size 705
