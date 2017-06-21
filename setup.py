11737e61688789705a95b6432aa030287e22bfdb
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-21 17:24:42 -0400

    Fixed README rendering on Pypitest Former-commit-id: 011c6d8628729110fcaec7fc34432c83adf1f615 [formerly cfa12941e3edc0a1bfe2c4e41ab5faf406d1c05d] [formerly 60ccc933a779611979fd55038d9b7978f4461260 [formerly 9ce8deafd241d551391b9bd4f42dd35d2ec68790]] Former-commit-id: 2e47a77b771935a037dc424f9b7d5cd918ff78bb [formerly 0db26fe6e868b547ec58fd97ca1bfe61188bfa11] Former-commit-id: 6ca453a778af2ee425c93cedd81d0895e1217516

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71666c9603a5db457bc20bee0872fd5c9be463eb6fc2d78f4f085df18a3febe6
-size 2725
+oid sha256:c69e0ceb11e8f8bcb0ba4c89957ff83f36ba3fb3e39898fa95135e9a57f28f4e
+size 2713
