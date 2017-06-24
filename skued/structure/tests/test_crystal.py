0631295773c43a738ab646aa23c5149e230912a7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-24 12:57:43 -0400

    Added a way to construct Crystal using included cif files Crystal now has another constructor, Crystal.from_database Former-commit-id: 716ac645127aaf26fbec9ed831e76274e1764c04

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d126c6f4e553c32ac2a67a06bec7c4d5db4783f902b4320bc815cb11648b9a4
-size 3067
+oid sha256:729f0cd0d5ff3738cea6b09b72e702c3ebb765a96befda8434de174b1df94d91
+size 3581
