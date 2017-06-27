01f12fc4f90d4fac5bd285612c2b2ef4173fbfa8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-27 08:21:35 -0400

    Added a way to mask images before alignment Former-commit-id: ef2984821939b7797a57e28504758834b4f0648c

--- skued/image_analysis/alignment.py
+++ skued/image_analysis/alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68289aba583b0b13bf5a4825b56f92b6e8b2865b8579b31a6f906b58e09f79a3
-size 2265
+oid sha256:56ce195134c4c72f2e8c180c66773933dc78a7d6cfe12f5f2bccbff26ed9cbad
+size 7442
