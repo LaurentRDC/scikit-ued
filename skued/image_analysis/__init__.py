c09a440c4ec67c151aeb9fe9eed60110c47fb6f0
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-29 15:38:41 -0400

    Revert "Added a way to mask images before alignment" This reverts commit 6547b3fdea6e1461cbc8bb7b8245fc583f244eaa [formerly 0f8915c04baa537d9b6bc4ced8b31bcbbba6fa2e] [formerly 749e049410c8a6a39d2631cacbcadbca3e790c63 [formerly ef2984821939b7797a57e28504758834b4f0648c]]. Former-commit-id: 8b7587cdaebf428ce725c46a14fc71220ec5dbf9 [formerly 39b44be2d30f82ff2a80e5d247931834b838b0e9] Former-commit-id: 01c3a25dbdc649b325561441b3c3c8fd4295d0cf

--- skued/image_analysis/__init__.py
+++ skued/image_analysis/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:641f5ea482065582274199f4a4c1a04fc961455973842178c556acd6ddf819ca
-size 211
+oid sha256:8f5fb5d3409bce5ae0ba2cbb2cee6b4e47f1cc11e8a66a0328156838f143ae7b
+size 189
