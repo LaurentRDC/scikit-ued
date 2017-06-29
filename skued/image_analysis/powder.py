ec5cd9ae7dd6962af093ad8a0e54987bab9dd51c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-29 16:26:24 -0400

    3x performance increase for angular_average Former-commit-id: f9bb0924ca43aa3c09bd80594f940b614904ed99 [formerly 3ca06fbb9e0b24da36d415a11fdf0ec5f79d0f72] Former-commit-id: 397440f9d5b99519a5fc02361d79107d184122f7

--- skued/image_analysis/powder.py
+++ skued/image_analysis/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eca73918e2a836c5011c133362a82376df30550ecaf4b202f5d55c4450b6c6b
-size 4053
+oid sha256:3e039a5b99a915cc9e5ce04cfbb585acd27c704e58f948e6044d3e941eedd973
+size 4037
