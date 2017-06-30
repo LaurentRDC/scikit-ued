3407b8c60be0266136ad1c5f4865f422d3788674
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-06-30 08:56:10 -0400

    2X speed improvement on angular_average Wasteful use of meshgrids and numpy.unique have been removed. Former-commit-id: 5afd023f6e004cd5c7378bb2a3a949ac7d89016e [formerly 32f94c817b7310ee1fd0f5821f3998778da57589] Former-commit-id: c139d8ece7ea23540b768f03324a088b35fcebce

--- skued/image_analysis/powder.py
+++ skued/image_analysis/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eca73918e2a836c5011c133362a82376df30550ecaf4b202f5d55c4450b6c6b
-size 4053
+oid sha256:30ccf652b0299e08f0665040b18286dd463bb43d20c05daba45f34a5c6fa6d31
+size 4174
