96128016024ccc4d59114d24c09e390a4ea805ba
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-03 11:03:56 -0400

    Added recipe in streaming tutorial Former-commit-id: 68a1418bf968a407a8fd9deb15ac73286ba043a9 [formerly e60d6d9127de6db5d2cb32c8f5cf3e98deafb0c1] Former-commit-id: 5ffbb1b06f5e5347dbb315a31e312b9d4f416cc9

--- skued/image/streaming.py
+++ skued/image/streaming.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9a2792aa0165da36a9f8444b63f3c99b5cc35ba9aec6b2ab8b5cf490413e2fd
-size 3611
+oid sha256:e97fb1ec4f0bb15018b0b7d5f8943bf85ecf5969541bc61d625998aaff74bda9
+size 3621
