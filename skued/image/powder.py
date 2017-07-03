39e02663fc02f2a83eaf52da507a35d3e89a22e0
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-03 10:02:23 -0400

    DOCS: updated documentation Former-commit-id: 57779e3c0224294d16df4988fb5486e890c35bef

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea388397d28e2063f29e6e70f285d6054a7c31a8732da8bdd1067b01099ff748
-size 3880
+oid sha256:8d04bc8418e2fbb27d701a4576362a645bea025721ba7da7c41edfc1360cfa76
+size 3921
