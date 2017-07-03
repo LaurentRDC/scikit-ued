8e42c51d72d9688bdff1396649a6b39e0dab3fe2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-03 10:02:23 -0400

    DOCS: updated documentation

--- skued/image/symmetry.py
+++ skued/image/symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc89b9ff97487ebcabe521e3da258c1b815902b5c13b25f9a965c1354ef70dfe
-size 1070
+oid sha256:b9bdf9417bc6e50dfe6237cf87f74f37c40bcf70586da377b342cb8001734ca7
+size 1108
