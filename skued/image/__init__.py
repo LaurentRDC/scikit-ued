3ed1e244c9caac2bb57ef0dad1fa0f184da0cc93
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-04 17:15:04 -0400

    Added a register_translation function using masked, normalized cross-correlation Former-commit-id: 1d29232ffc25497d7de267dc397c968828ce9e9b

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f0a79ccdb197988d25347ca2614eb9a2e42ea3b2a12b9ba5c7b8c83287fbe11
-size 287
+oid sha256:4c7bb6b632a971a9fbfdffadfaf90de44d4938793fba5fc04492ef29b4fc4cb4
+size 309
--- skued/image/correlation.py
+++ skued/image/correlation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46dd32db0337a9eef282d183755dc9eed4ee0c30b7c47ccbfcc49addfad1c9d9
-size 3577
+oid sha256:009fdc6968c363d70fe364bc8648a1b33f62241ae0e1652b87d21669a3c0f89b
+size 4730
