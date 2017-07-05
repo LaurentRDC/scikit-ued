120c6a61b7f679343d2756e0ed1099f8deabaa2d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-05 12:54:00 -0400

    FIX: correlation test size Former-commit-id: 00f469a7c6a9d34908fc11b51a2973a3c78f5a9b [formerly 84060d289682f3f567f6391349532e9a2cfa37b2] Former-commit-id: e764ebea4af27c4214b7f5576bbfd43cabddd0e8

--- skued/image/tests/test_correlation.py
+++ skued/image/tests/test_correlation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b81bb350adda5ccc0653e273406f7a1a3caf16f60e9588eb57e85c360780317c
-size 1756
+oid sha256:1e2b4c0a0746fddedd547c138799ad053e2a8fd2886cf8076692c201482a8164
+size 1754
