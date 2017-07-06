ac4f9bae2c7606cb38a36867d23bcc5c4ec05816
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-06 12:28:02 -0400

    Improvements in alignment of diffraction patterns After testing on Chromium data, this commit switches scikit-image's register_translation for scikit-ued's diff_register for aligning diffraction patterns. Image tutorial was also updated Former-commit-id: 5dcce3f1754ea94adf1bfea3785bd520867eb972 [formerly 4072d78fd585fb679783680eee4d92885cb95d88] [formerly fdfa047653266c76e996daca890f98e2164d8efa [formerly e15ddf4ed8c15408e4097006fd90cd5b9beed26f]] Former-commit-id: c30bbe574c702c5f9765b21ba29ce57e35a6f74b [formerly 3f675934874a4e60b4a5dbbdcab159e1a206524d] Former-commit-id: e13680195fe40a2acb142115f0a53eb94550dd3c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ff3a96fa4974afcb4d19559717d2436631f38a81ca48234377a6bd91d6d87d5
-size 4346
+oid sha256:58b954f2b6ad6048872a1b7f3dbcf2f7c9a904564bae8ad5ba20273087b591b0
+size 4400
