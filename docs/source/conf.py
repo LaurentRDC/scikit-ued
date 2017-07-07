934a89929f521fc309b83a6950389a7f88554ae2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-07 13:32:48 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into develop # Conflicts: #	docs/source/tutorials/baseline.rst Former-commit-id: 29ee6146d004cb1bce60d52742399a04632cf5a8 [formerly f0b4fb0a99433f46c91c217bdf84bc5349bb4582] Former-commit-id: 31deb64ff170f265a87dae19edc3eb8a6c0e7123

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b954f2b6ad6048872a1b7f3dbcf2f7c9a904564bae8ad5ba20273087b591b0
-size 4400
+oid sha256:e4688430f7d62032bee43c820d4030bddebacb23f3e9175291070f5c1ff44584
+size 4347
