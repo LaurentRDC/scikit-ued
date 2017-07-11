affd3695d3a21e2dc2d2625ac0476fdbdb2db842
LaurentRDC <laurent.decotret@outlook.com>
2017-07-10 20:31:48 -0400

    Included hex to RGB conversion in rgb_sweep Former-commit-id: f8b6575b19774dcaa47c3be6654527839ed6489d [formerly a916cb8d01d3e993d841826361efffa0f23503ee] [formerly f13314fa84e33ac882c4e9c49f7602ecbb6a9e06 [formerly 40a909440b68306b517ba536f2616fedf06e33c0]] Former-commit-id: aecb878de98702083cedde302bdb4e1eba50bc87 [formerly 94ddc1fcefd91afe7d2d13716924ee3c6831c72e] Former-commit-id: 40e85b18849c2dc5b74c3efc4cde84df0acab578

--- skued/tests/test_plot_utils.py
+++ skued/tests/test_plot_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c320424177eacf436a9d76b034cea562f760d9bb5678d319b4aa6a16d637fa6c
-size 1356
+oid sha256:5d5a4ec36f289f8fd3278e182c2b06800b92a8ceaf181f015f805198a502d8a0
+size 1626
