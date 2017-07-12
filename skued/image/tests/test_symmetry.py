cdef6b635853b32273c9633f1a76472f9e383ac7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-12 08:48:08 -0400

    Added the possibility to mask images in nfold Former-commit-id: 6e3b15a112799c38d8f7b4c8feab6c9db41bb1a1

--- skued/image/tests/test_symmetry.py
+++ skued/image/tests/test_symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:129e041ace64357557e9bc400be3a4fef78ff262d6d014a18758c3d5a6f1efb5
-size 665
+oid sha256:244fb35d423736692601984c1d88f35c18147b122e4f3e69a47bc33d30c04588
+size 1282
