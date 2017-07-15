bc89098309d05799cb7e0076ba657af334237eb7
LaurentRDC <laurent.decotret@outlook.com>
2017-07-15 19:30:02 -0400

    More streaming statistical functions in skued.image I have added streaming equivalents to numpy.std (istd) and numpy.var (ivar). isem has also been refactored to take advantage of those, adding an extra parameter (ddof)

--- skued/image/streaming.py
+++ skued/image/streaming.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5a5326444ca878dfee0172a03efdc0297fa7f918adc01e8c027412965f3cebd
-size 3371
+oid sha256:2011c4d2570322d6c62b462efdeb5a387253ca88c0f9b937ff80b9487ae6cb7b
+size 5259
