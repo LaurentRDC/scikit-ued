d71585e6c03e871bb874514ffcd607cb2aace99a
LaurentRDC <laurent.decotret@outlook.com>
2017-07-16 09:15:30 -0400

    DOCS: improved documentation for skued.image.streaming module Former-commit-id: 441fa51b96dba86bfdf22a4e15cfb418ece59e89

--- skued/image/streaming.py
+++ skued/image/streaming.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4febb0a87a4574915058c2d49c666594ef3dfd661f120dd243963184242b369d
-size 6796
+oid sha256:8bbfb69747ed93be691933aa8e6ebbec8f4ba9d13e0c9d15de4157068aa6d8f6
+size 7063
