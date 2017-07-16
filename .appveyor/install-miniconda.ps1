d187b5be1a281a95d30ca7660023f55d909120a1
LaurentRDC <laurent.decotret@outlook.com>
2017-07-16 11:04:20 -0400

    Made Conda-requirements and pip-requirements global in appveyor builds Former-commit-id: 51df972b0cf479c60c80987b95136e21dc0eaaaf

--- .appveyor/install-miniconda.ps1
+++ .appveyor/install-miniconda.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f4e0258bb4ebcc0b9d6eabcb436cfece145ad227e6d04858746d129760b02bf
-size 5862
+oid sha256:a056844ed04d45d6a930cfbfcc2359b89cdd7d6a40045cf075c0ab73137a7da2
+size 5727
