24ca2a7874fac70824e5cfb624807d99eaa26a46
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-18 17:26:23 -0400

    Reconciled Atom frac coords and real coords Atom now have a lattice attribute, which is changed to a Crystal when crystal is created. Former-commit-id: b4d178a4f2e3b205cdea8cc95079f04e035a0b2c

--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:264935d63dad5aef976fcf0ec4a85beff3818b236d77a4a6f57d80606ba4fc95
-size 5124
+oid sha256:f28b9884c189fccb5adafab171668cb628db13250a68027b769bf030959a0ba7
+size 5159
