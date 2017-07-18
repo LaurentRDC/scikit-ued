689bdcca6bf60f2b2ffb2ccfcfd25317da9bbb3c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-18 17:47:47 -0400

    Using random Crystal for testing

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc16aaede6f14c4039f0eed6b10a0f22e44e5fdef9dbf6eb9aeefd91a79dd548
-size 3933
+oid sha256:c0f7ed362fb2ba9875269149d49500a86e93d183ebf3b429955fabb2f32f977a
+size 3623
