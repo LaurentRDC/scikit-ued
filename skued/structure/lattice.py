b2b7b4ff1fa7f64ce055c5d93cc45a0b6b71e31c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-19 08:24:06 -0400

    FIX: recursive call to Crystal.__eq__ in atom.__eq__ Former-commit-id: d5d9392a9ef4682e8cde5730d8a313e80c0232da

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df3292d84b8cd6ea3a3b9a7ba7bc64fa0849f7111fb821ea612ff939d0f7c164
-size 4065
+oid sha256:2597fa03f2ad59d9ca201ef9f47dbe084e47f46efa765c2d36449a1a00e0af3f
+size 4185
