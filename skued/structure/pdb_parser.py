a94686cd8fe5321f56ddb2de308fbd74cebadf14
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-19 15:33:08 -0400

    idgaf

--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f28b9884c189fccb5adafab171668cb628db13250a68027b769bf030959a0ba7
-size 5159
+oid sha256:2b4088ca104f45a36d78f09174ff81d3159251cdf7ba72f3a92bfe0a5e455ba4
+size 5830
--- skued/structure/tests/test_cif_parser.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b1d9b97dcb708e68a913cefe0398e2d5f64f5de235cc80871064357081c4759
-size 2914
