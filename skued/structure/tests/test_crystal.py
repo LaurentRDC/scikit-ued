6d780e4541855497477ead6a261dd5ac4ccb4d9a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-19 15:33:08 -0400

    idgaf Former-commit-id: 25533df96dab203c8ac74feaf73094b8c1d02e5f [formerly 9c4c1f6b402261ac068128e9a9223ca5d45976f0] Former-commit-id: 83484410e94597cc31f35bff642c7403d477808b

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d38d49f067a8f15920578eb010b15e17a87ea4e7f0f624c2be5aecfb7a190c43
-size 3998
+oid sha256:03497bd029c0091366714f17c47fb9d3ab6fba73b8e2cec970052a3f6c9cfbc4
+size 4218
