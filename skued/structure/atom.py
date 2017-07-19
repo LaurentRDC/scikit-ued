22b4699a4b0ad2ce0ca7674e5d06b3e5c1eb7be5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-19 15:33:08 -0400

    idgaf Former-commit-id: 9c4c1f6b402261ac068128e9a9223ca5d45976f0

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b1e4d6bf9d0da7f939a4c36376bfef1e91fb7b2ee1e404b23448f714a09affc
-size 8234
+oid sha256:e53e3597b26d65800e8c73f99641492600a921f738bd1c53c2d97cffd639688d
+size 8265
