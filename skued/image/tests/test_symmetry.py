dde9a247d61a39cfdaa769bf4350f3e8393255d5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-20 08:46:20 -0400

    Unrelated warnings are now suppress in skued.image tests Former-commit-id: 4482bbeb4ae85a3d5cfe111f2282493ae0ce8853 [formerly a7905a93ce8380a7837a986e7d61909f7717c5cf] Former-commit-id: 027adda3f707391c4ca8094ff681d134abfa3eb4

--- skued/image/tests/test_symmetry.py
+++ skued/image/tests/test_symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9fcaec1586cbf2f9d62db43defac1939ede42a841cbde9486d3eb727f347063
-size 1939
+oid sha256:cf651723ce76d606e7db9cac6754a8340b66f66374739e8b1a129538cbd0714f
+size 2252
