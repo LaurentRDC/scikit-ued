4a68e7bfaff107b5f7c269917703b1cf852891d5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-20 09:00:20 -0400

    FIX: CIFParser and PDBParser both using ParseError now Former-commit-id: 206e8690044d1974bfa76af5aecf315ea92b2270 [formerly 610fdc47514a058da3c9919170483ba0103b4819] Former-commit-id: 43a315b81d4ec5bc70d4085e9d0426ebbfd0f7d0

--- skued/structure/__init__.py
+++ skued/structure/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:157b441e2c8f6cec357fbacb7d33a02bdf496b0fc830541b7a86c019808a71c0
-size 1059
+oid sha256:406d1b2310df3ee1890f36f5fdde047b9be5224c91c9da6857c9eee4c3fc15fe
+size 1100
