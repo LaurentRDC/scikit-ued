9817f04096ffd00a1e9694c2450c9dd4969aefab
LaurentRDC <laurent.decotret@outlook.com>
2017-07-21 20:54:38 -0400

    FIX: allow atom.real_coords to accept lattice vectors AND Lattice instances Former-commit-id: 0517aea6a20a3a18f42c2deb2296827930723c00

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e53e3597b26d65800e8c73f99641492600a921f738bd1c53c2d97cffd639688d
-size 8265
+oid sha256:8cf2ec27d2c804781157da03188cb04b5e1c33e6f25cc2a36e4ffaf9d8b34321
+size 8413
