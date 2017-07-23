3c0468394356e60c41f16ed2288592d67b6bd643
LaurentRDC <laurent.decotret@outlook.com>
2017-07-23 13:28:17 -0400

    DOC: fixed documentation issues related to lists

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6af0bc8c75d7c21dbc86843bb7eb4c1b52516655c558588e5a57ff2d9604ae74
-size 8562
+oid sha256:cf72e0c0caa2f6453c7f883acadc1deed1a3dc1863bc2ea75d8488086f704836
+size 8442
