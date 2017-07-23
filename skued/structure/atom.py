bb621d88f8c54a9a42e0a93b75e624818168a3c8
LaurentRDC <laurent.decotret@outlook.com>
2017-07-23 12:40:41 -0400

    Added the ability to create Atom from either element or atomic_number Former-commit-id: 9d31257936a9b683efd045b5e6fd23b192aef729 [formerly db4da8e5429400600bcdd716903131e1a5460bb4] Former-commit-id: 64eee9366bc5cb302ba979641224b537bbc69e32

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cf2ec27d2c804781157da03188cb04b5e1c33e6f25cc2a36e4ffaf9d8b34321
-size 8413
+oid sha256:6af0bc8c75d7c21dbc86843bb7eb4c1b52516655c558588e5a57ff2d9604ae74
+size 8562
