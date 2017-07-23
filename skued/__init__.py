32f285bcefc07ca177107b277a7879b8b353f571
LaurentRDC <laurent.decotret@outlook.com>
2017-07-23 12:43:01 -0400

    Exposed Crystal, Atom and Lattice to base package Former-commit-id: ce84b66a1d7a8d1db779bcef82b8bfafbaeb4c45 [formerly d9f81636af751485c03cf4d77198cf4398f5ec74] Former-commit-id: c1ecdd85fe9edb1ae1e8b588722847749f348f06

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8164ba8fc9cfa0c8475682ed6fada472b7aede90a09dd557105ed0582701a6e6
-size 779
+oid sha256:b6fd83951533f47d863ba118637cbd32b03ea27a0e4ce3078aea05d397f6a1c8
+size 825
