0c191c506819d91898db1eef01cbd4c69ab7fd94
LaurentRDC <laurent.decotret@outlook.com>
2017-07-23 13:28:17 -0400

    DOC: fixed documentation issues related to lists

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fbee2105052c723261630eab2044e3405c93f69a2c2805733a6971f44c85265
-size 13859
+oid sha256:798fbb1d6085d47e5c9dba76a67e577338111efd0fb5238ec24dc9b2bd1aac65
+size 13687
