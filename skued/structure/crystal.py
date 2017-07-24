860151678025df156f56e124d0a11e5fafd3a8b4
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-24 12:47:49 -0400

    Updated functionality for Crystal constructors Former-commit-id: 2afaaa329fa67942099ff0aa4aa3d5bcce9e2ed2

--- skued/structure/crystal.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4e85b75f2360dec5a00dd72deb71c3f7a6791c9b18602eeb5e6157360378b919
-size 17787
--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:059e88e9c287571496b7dba8860063aef502cf4faec0a0c5ee2d28667fad3552
-size 5733
+oid sha256:b68ed837f23f5f8a98fed8c4a934b7dd25a83cf5a5ba8a773de1ab2a4e3f9e37
+size 5707
