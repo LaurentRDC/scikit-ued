208e1d0c1d1022b7a3ff94a7b829d901d1b25ebe
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-24 12:47:49 -0400

    Updated functionality for Crystal constructors Former-commit-id: 2afaaa329fa67942099ff0aa4aa3d5bcce9e2ed2

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdc6126b0f0e4df2fc81bdb2fde3acc435b8e99fed728dca2de2528d33b126f3
-size 4862
+oid sha256:4da8b6fb1796875b44cc34da421d7086d2ec101f71292f6be8b173103158c756
+size 4920
