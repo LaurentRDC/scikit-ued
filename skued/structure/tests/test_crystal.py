6165cca6c77c94f81a1508936d658f36917654a9
LaurentRDC <laurent.decotret@outlook.com>
2017-07-23 21:12:18 -0400

    Added tests on space-group determination using spglib Former-commit-id: d8e9a5cb278f510ee12b03986da7eef63e7b8d4e [formerly 53f67da1d0c79b617ac97f38055e3d12b174a2f6] [formerly b27cfe68e1d35c98bef2b5634d7027a1f699d00e [formerly 15e2e875550129d64d631bab4d445c47bff63a4d]] Former-commit-id: 7dcc2aa206fa57dfde2ab50d687f11b7392e9a3b [formerly 320e9ebe94ef36c7a1d16ff472dc46025f4f6db1] Former-commit-id: 8db8519bccf53de5fe104e7e9106943a93517041

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03497bd029c0091366714f17c47fb9d3ab6fba73b8e2cec970052a3f6c9cfbc4
-size 4218
+oid sha256:cd83b5aa1bfa75192a00f8c53bf713469948f8bb1c063319c6a6553f44bd866f
+size 4259
