1ea09609eb1fef4c5ea2c4831183e89d895d88f3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-24 09:21:03 -0400

    Added 'pointgroup' and 'international_full' to Crystal.spacegroup_info()

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8adcf1062d32f273c84e99104b814bef7b25592b96e98483ee4e242d789c982
-size 4739
+oid sha256:fdc6126b0f0e4df2fc81bdb2fde3acc435b8e99fed728dca2de2528d33b126f3
+size 4862
