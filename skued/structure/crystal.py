f4c750418e4eb8c11dd9b538edb533fe957adc8f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-24 12:47:49 -0400

    Updated functionality for Crystal constructors

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e85b75f2360dec5a00dd72deb71c3f7a6791c9b18602eeb5e6157360378b919
-size 17787
+oid sha256:204da469d96d8b40bbee553817b65aa5dc381887f034430d9d9534684ebf9f28
+size 18494
