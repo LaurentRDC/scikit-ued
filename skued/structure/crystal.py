8944cd59e277084b82cc0bbc1ffac485bcf4afca
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-24 12:19:24 -0400

    Added warning that crystal from PDB might have incomplete info

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2a85a3e1bb938934bcde8884b7259873041e0a80fd15cd7520aa0b900a290f5
-size 17476
+oid sha256:4e85b75f2360dec5a00dd72deb71c3f7a6791c9b18602eeb5e6157360378b919
+size 17787
