d8209923b9aa0a4df76658ced95d084faed10c4f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 14:14:57 -0400

    FIX: atom with 0 magnetic moment could have magmom = None Former-commit-id: 6bb907429b36cd0ffe1f7cc56027ba10ed7ebd3c [formerly ec06b45eaf881b2e12b4f8ccde30d59bfc471f13] Former-commit-id: 26935cce812ac030304a64896bf5f575cf81055f

--- skued/structure/tests/test_parsers.py
+++ skued/structure/tests/test_parsers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a9267f15218dd92d6478ed1089cf78c5bd58a6cc27549cf0662ab634d6a16b7
-size 4488
+oid sha256:a5eded1bc1704f6d1b9b060b438ffab4b237b106b3a2fcb92feb9c21e2328cbe
+size 4412
