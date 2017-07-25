74c9afd412271304f3b024d5180353f21a97395b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 08:47:32 -0400

    Added conversion between skued.Atom and ase.Atom

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:542cf2e7ddc411c9674df00b573b9eefde9716db2279bf00eedb2601d554e207
-size 19452
+oid sha256:3dec75bfcb75427f030b6b51f38a9ae10cd09fdb6ca50bcbfd032c6a9209fae8
+size 19256
