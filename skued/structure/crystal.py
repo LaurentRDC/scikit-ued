a79c925d3622709e69184d0d5718fa203fc56960
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 14:25:15 -0400

    FIX rounding error in conversion to ase.Atoms Former-commit-id: 83e1c4a95f13f790903e15c251987323d6c33d57

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41dd83206b8a4212af0c880db29791642f2e284e470e64c4adc0ed1a392ab6cf
-size 20778
+oid sha256:be7d724c953e17889a53482f6e4f6cdfb1cc7430f06843f8d2e70d034964ee5d
+size 20761
