e145c92cfc40e0fd0efaaad7840714787b8b49a7
LaurentRDC <laurent.decotret@outlook.com>
2017-07-24 21:40:23 -0400

    Added barebones ASE compatibility Former-commit-id: 7d3f332fca54deb89aba5f5c5480cc8a6f9b9088

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf72e0c0caa2f6453c7f883acadc1deed1a3dc1863bc2ea75d8488086f704836
-size 8442
+oid sha256:490e0722f127400823da3797b5177ccf3652fd3d4e0aefc056401504da65953c
+size 8451
