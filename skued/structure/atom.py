774961e1051f22734892f1df6ed1f244403b3de3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 08:47:32 -0400

    Added conversion between skued.Atom and ase.Atom Former-commit-id: c971d79a1e710df524e242e80a6c1cb0ce9a2a02

--- skued/structure/atom.py
+++ skued/structure/atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:490e0722f127400823da3797b5177ccf3652fd3d4e0aefc056401504da65953c
-size 8451
+oid sha256:db85a5497ec19e0e55030ac2467d91d264d33930c2a0aed82d07a4470e20fa40
+size 8685
