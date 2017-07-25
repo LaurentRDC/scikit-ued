903cbab251331d86b90fbe437a19080e7f9c18c0
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 14:25:15 -0400

    FIX rounding error in conversion to ase.Atoms Former-commit-id: 8174f1f27cb2d79a681046a9738ce148c1e57661

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:891bc2f5a334c1ba935896ab9931f5f5e8f1d945e8c8377dd0b361609817e114
-size 967
+oid sha256:b6fd83951533f47d863ba118637cbd32b03ea27a0e4ce3078aea05d397f6a1c8
+size 825
