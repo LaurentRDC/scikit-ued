c257ad164a3bf35874cc7dc9c7db83cf7e6f101e
LaurentRDC <laurent.decotret@outlook.com>
2017-07-24 21:40:23 -0400

    Added barebones ASE compatibility

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:204da469d96d8b40bbee553817b65aa5dc381887f034430d9d9534684ebf9f28
-size 18494
+oid sha256:542cf2e7ddc411c9674df00b573b9eefde9716db2279bf00eedb2601d554e207
+size 19452
