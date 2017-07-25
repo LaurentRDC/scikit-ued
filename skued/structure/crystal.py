fbdaa0ac868a10f739f41e7cb9eee5049def1e13
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-25 18:03:23 -0400

    Removed reliance on symmetry operators There is no use-case in which access to symmetry operators is important beyond building the Crystal object. Former-commit-id: 8c70bfd81ed21338a8cc7515c9ed18b091282895

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be7d724c953e17889a53482f6e4f6cdfb1cc7430f06843f8d2e70d034964ee5d
-size 20761
+oid sha256:fac79e2fb4b8fec563e06fe157c93bbbe4835e9bf824e092ee36f4e76557bf65
+size 20592
