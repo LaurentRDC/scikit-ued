50f5226c4046aa8bfaa4dfdbed5294d2d9689b36
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-27 14:34:32 -0400

    DOC: better documentation for potential.p

--- skued/simulation/potential.py
+++ skued/simulation/potential.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8d6a77f9df50fd93bbe78e698516ee2f4b1dc5afdc67613862edd271cb07797
-size 3762
+oid sha256:4ac21a2b67babae8fd15460732ea57a4785079e4644b7c304e2fd716f6a1b9c8
+size 3521
