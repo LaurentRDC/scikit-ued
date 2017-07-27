8b6bf4bc53e9ffc4796d509dd6cf14498d128420
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-27 15:41:43 -0400

    FIX: array shape in pelectrostatic Former-commit-id: a741419873698c3ccfde0be53686fe13c9f12d46 [formerly 462f7e1926a7423f08f395be722834a344d1bca4] Former-commit-id: f549ef15856087a095090a899e08a16f74f1c5ef

--- skued/simulation/potential.py
+++ skued/simulation/potential.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ac21a2b67babae8fd15460732ea57a4785079e4644b7c304e2fd716f6a1b9c8
-size 3521
+oid sha256:e3aff3398f495a4c878318ee0409c17b9974331fce1f54c0e589e83f50e6a6be
+size 3504
