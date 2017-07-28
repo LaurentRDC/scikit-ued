096e7e1d4f0f7caff9c3ea62715c655d05ad9ac9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-28 07:43:23 -0400

    Move periodicity ot Lattice Former-commit-id: 6552f2d1c6d4fd5ca05e96cc2537b25194ec0222

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45dfa34a8fdc51b1a1c0afbeea622363653901a7ca360bdf98b149d678fcd214
-size 4256
+oid sha256:3efe9672ccd7e1674314be67e308650ed807cd7a9eacaab9b6d71a5718cb5f88
+size 4785
