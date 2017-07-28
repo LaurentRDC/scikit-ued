c4d2735611e1bbfb73fe6c37e168a22b35913182
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-28 13:04:05 -0400

    Added __array__ to Crystal

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eefbfa6e66c39c78ec48691b193667209c76b434ff23555c2672acf7abeb7c51
-size 18701
+oid sha256:262f0c1a2c29a464016fb8539a938409cc4c2edec070c24ef5c7acd1b64c071b
+size 18906
