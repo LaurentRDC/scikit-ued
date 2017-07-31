019aed88c19e4c815f9536b89cbe54420c9d6668
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-31 15:11:36 -0400

    removed streaming module in favor of npstreams Former-commit-id: 3df5457c254796e9c22d5f9c676070807906e0d4 [formerly 84eead93bde8f10980df2652ba0041a8b22dc932] Former-commit-id: 941609ecc07748db3e7601816b7ebcaae7c1de28

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddbb66bb616927a271d58d0bcc064cedbd03d5db7f0f0ac0f419b81ecede85b5
-size 300
+oid sha256:fbb9a0b0da292488ddfe9cb4b34ed08206c280ab81f40c894db7a724c717e3fb
+size 250
