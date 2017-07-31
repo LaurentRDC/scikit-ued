5b473af11c67dfeb9aa7123b3c5bbe578f9408a9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-07-31 15:11:36 -0400

    removed streaming module in favor of npstreams Former-commit-id: ca7dfee9b35bac9590c7bece563501fe29dfe0cc

--- skued/image/tests/test_alignment.py
+++ skued/image/tests/test_alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bacbf65ca5a9110fa0802585fdb684091583a271d4c7f71ec833a4fcf11ed45
-size 4912
+oid sha256:e6525a5745828d3d5e72bca4e791a26a6fce4fa13646c7dc2926909f28917e5f
+size 5848
