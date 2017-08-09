70ec6a3adbbd2268eee0e95790c29e2161a2e04e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-09 17:01:17 -0400

    tests on sim_mesh on smaller meshes Former-commit-id: 84a33ab0a6c66317af711be4a6cfd53f23e61779 [formerly abd9a038a8f8c343204806e41c41a8ba13c657e8] Former-commit-id: 10737b814e44213e223e4c5d198c017d7a2212f7

--- skued/simulation/tests/test_multislice.py
+++ skued/simulation/tests/test_multislice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0af97b0414aed40e4c03fec993b0fa1ccb2cfae3f62054828621d4bbc194b8f
-size 1789
+oid sha256:9f7b47190482ed3ade2ccc8f5c037ed94224a077af0c1a041e0f33ee57607c90
+size 1773
