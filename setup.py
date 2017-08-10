5ef868ca1327d35b560412694b229a11e299f043
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-10 12:35:58 -0400

    Updated setup.py for clarity Former-commit-id: 2d3505d77c5ce3d077e0568fcfd21d43ce7c05b6

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1464e211a9cdc4755b8e5aaf5b42bcbe94ea8b1424fb24d33002c658e081bc6f
-size 2743
+oid sha256:e043285c1024d1f4cd096901769db0664e4ab02bb578bebb8c193be2bb6e4ef5
+size 2888
