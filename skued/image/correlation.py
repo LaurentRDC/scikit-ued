200ddb124274871c260b98deb0b7844fea9ddf12
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-10 12:23:36 -0400

    Added options for fftw Former-commit-id: 198fe263a7abd6a3b3cb52bd1dfe72cbb5c8885e

--- skued/image/correlation.py
+++ skued/image/correlation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a50e47b699363e4ad2c69a7435d466017767b1c6fd6baab6c6d1da3fe947535f
-size 4422
+oid sha256:57b25ab3501f38c47da03d9f37602226eeea5fa5a00d89d4c0ae507e7211cab7
+size 5114
