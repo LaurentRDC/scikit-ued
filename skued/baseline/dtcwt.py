13b0369c4cfb3063aa6f9f3dbe3094186dacffe2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-14 14:33:53 -0400

    DOC: updated documentation for baseline module

--- skued/baseline/dtcwt.py
+++ skued/baseline/dtcwt.py
@@ -10,31 +10,31 @@ from functools import lru_cache
 from os.path import join, dirname
 
 DATADIR = join(dirname(__file__), 'data')
-ALL_QSHIFT = ('qshift1', 'qshift2', 'qshift3', 'qshift4', 'qshift5', 'qshift6')
+ALL_QSHIFT = frozenset({'qshift1', 'qshift2', 'qshift3', 'qshift4', 'qshift5', 'qshift6'})
 ALL_COMPLEX_WAV = ALL_QSHIFT
-ALL_FIRST_STAGE = tuple([wav for wav in wavelist(kind = 'discrete') if wav != 'dmey'])
+ALL_FIRST_STAGE = frozenset([wav for wav in wavelist(kind = 'discrete') if wav != 'dmey'])
 
 def dtcwt(data, first_stage, wavelet, mode = 'constant', level = None, axis = -1):
     """
-    Dual-tree complex wavelet transform, implemented from [1], in 1D, along an axis. 
+    1D Dual-tree complex wavelet transform [1]_ along an axis. 
 
     Parameters
     ----------
     data: array_like
         Input data. Can be of any shape, but the transform can only be applied in 1D (i.e. along a single axis).
-        The length along the axis must be even.
+        The length along ``axis`` must be even.
     first_stage : str
-        Wavelet to use for the first stage. See skued.baseline.ALL_FIRST_STAGE for a list of suitable arguments
+        Wavelet to use for the first stage. See :data:`skued.baseline.ALL_FIRST_STAGE` for a list of suitable arguments
     wavelet : str
         Wavelet to use in stages > 1. Must be appropriate for the dual-tree complex wavelet transform.
-        See skued.baseline.ALL_COMPLEX_WAV for possible
+        See :data:`skued.baseline.ALL_COMPLEX_WAV` for possible values.
     mode : str, optional
-        Signal extension mode, see pywt.Modes.
+        Signal extension mode, see :data:`pywt.Modes`.
     level : int or None, optional
         Decomposition level (must be >= 0). If level is None (default) then it
         will be calculated using the ``dt_max_level`` function.
     axis : int, optional
-        Axis over which to compute the transform. Default is -1
+        Axis over which to compute the transform. Default is -1.
 
     Returns
     -------
@@ -47,22 +47,21 @@ def dtcwt(data, first_stage, wavelet, mode = 'constant', level = None, axis = -1
     
     Raises
     ------
-    ValueError
-        Raised if axis argument is invalid (e.g. too large).
+    ValueError: Raised if axis argument is invalid (e.g. too large).
     
     Notes
     -----
-    The implementation uses two tricks presented in [1]:
-        `` Different first-stage wavelet ``
-            The first level of the dual-tree complex wavelet transform involves a combo of shifted wavelets.
-        
-        `` Swapping of filters at each stage ``
-            At each level > 1, the filters (separated into real and imaginary trees) are swapped.
-    
+    The implementation uses two tricks [1]_:
+
+        * Different first-stage wavelet: The first level of the dual-tree 
+          complex wavelet transform involves a combo of shifted wavelets.
+        * Swapping of filters at each stage: At each level > 1, the filters 
+          (separated into real and imaginary trees) are swapped.
+
     References
     ----------
-    [1] Selesnick, I. W. et al. 'The Dual-tree Complex Wavelet Transform', IEEE Signal Processing 
-        Magazine pp. 123 - 151, November 2005.
+    .. [1] Selesnick, I. W. et al. 'The Dual-tree Complex Wavelet Transform', IEEE Signal Processing 
+           Magazine pp. 123 - 151, November 2005.
     """
     data = np.asarray(data, dtype = np.float)/np.sqrt(2)
 
@@ -88,31 +87,35 @@ def dtcwt(data, first_stage, wavelet, mode = 'constant', level = None, axis = -1
 
 def idtcwt(coeffs, first_stage, wavelet, mode = 'constant', axis = -1):
     """
-    Inverse dual-tree complex wavelet transform, implemented from [1], along an axis.
+    1D Inverse dual-tree complex wavelet transform [1]_ along an axis.
 
     Parameters
     ----------
     coeffs : array_like
         Coefficients list [cAn, cDn, cDn-1, ..., cD2, cD1]
     first_stage : str
-        Wavelet to use for the first stage. See `pywt.wavelist()` for a list of possible
-        values
+        Wavelet to use for the first stage. See :data:`skued.baseline.ALL_FIRST_STAGE` for a list of suitable arguments
     wavelet : str
         Wavelet to use in stages > 1. Must be appropriate for the dual-tree complex wavelet transform.
-        See dualtree.ALL_COMPLEX_WAV for possible arguments.
+        See :data:`skued.baseline.ALL_COMPLEX_WAV` for possible values.
     mode : str, optional
-        Signal extension mode, see pywt.Modes.
+        Signal extension mode, see :data:`pywt.Modes`.
     axis : int, optional
         Axis over which to compute the inverse transform.
         
     Returns
     -------
     reconstructed : ndarray
+        Reconstructed array
 
     Raises
     ------
-    ValueError
-        If the input coefficients are too few
+    ValueError: If the input coefficients are too few
+
+    References
+    ----------
+    .. [1] Selesnick, I. W. et al. 'The Dual-tree Complex Wavelet Transform', IEEE Signal Processing 
+           Magazine pp. 123 - 151, November 2005.
     """
     if len(coeffs) < 1:
         raise ValueError("Coefficient list too short with {} elements (minimum 1 array required).".format(len(coeffs)))
@@ -138,9 +141,10 @@ def dt_max_level(data, first_stage, wavelet, axis = -1):
     data : ndarray
         Input data. Can be of any dimension.
     first_stage : str
-        Wavelet used in the first stage of the dual-tree cwt. See pywt.wavelist() for suitable arguments.
+        Wavelet to use for the first stage. See :data:`skued.baseline.ALL_FIRST_STAGE` for a list of suitable arguments
     wavelet : str
-        Dual-tree complex wavelet to use. Argument must be supported by dualtree_wavelet
+        Wavelet to use in stages > 1. Must be appropriate for the dual-tree complex wavelet transform.
+        See :data:`skued.baseline.ALL_COMPLEX_WAV` for possible values.
     axis : int, optional
         Axis over which to compute the transform. Default is -1
         
@@ -246,8 +250,7 @@ def dualtree_wavelet(name):
 
     Parameters
     ----------
-    name : str, {'qshift1', 'qshift2', 'qshift3', 'qshift4', 'kingsbury99'}
-        Valid arguments can be found in dualtree.ALL_COMPLEX_WAV
+    name : str
     
     Returns
     -------
@@ -281,7 +284,6 @@ def dt_first_stage(wavelet):
     Parameters
     ----------
     wavelet : str or Wavelet
-        Wavelet to be shifted for first-stage use. Valid arguments can be found in dualtree.ALL_FIRST_STAGE
 
     Return
     ------
