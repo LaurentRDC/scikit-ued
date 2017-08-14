8b5b22c4499071d413c3dfb84b713d5cbaab2c8b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-14 10:56:33 -0400

    Updated name for multislice Former-commit-id: 4b91b5a526ad000a6a859984aad83dce19a880ad

---
+++ skued/simulation/tests/test_diffsim.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f567a8e7e91660991d035ad1b0e4923a0fb7d9277dd64d39dfc8914a74c52c6a
+size 3216
--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262f0c1a2c29a464016fb8539a938409cc4c2edec070c24ef5c7acd1b64c071b
-size 18906
+oid sha256:cf9b485232b606f201aa1d95039636b5cbd54a44be9b04a79acfeb2e749f815c
+size 18977
