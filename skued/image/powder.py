af9d7ecebad0f14a6f47347e560f277bc38a9f6f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-16 13:38:50 -0400

    Clarity improvements in angular_average Former-commit-id: 3e6aff4989f573ef24771f6e1686c38247aafd45

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62384fc1564faf7cbe905fb47d6b2c71950cea44c47406923197e4cf4ad362c4
-size 3763
+oid sha256:9956ba8b99f362320bbceebf4d88fab702a07532f4cc48852e9bed36304d879c
+size 3907
