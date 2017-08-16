f98d21e4dfe9bf51c7e8cbaefacae70f094ee0bb
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-16 13:38:50 -0400

    Clarity improvements in angular_average Former-commit-id: 0fbc138b3d6d895ac252f9d3634b412412100faa [formerly 3e6aff4989f573ef24771f6e1686c38247aafd45] Former-commit-id: 0c199ede20e6fd7668d638b44984b11a002befc9

--- skued/simulation/__init__.py
+++ skued/simulation/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b35c3d9a7ea4cad0633286a257d883e23912d1187cc6d80574c8b8327c1d086
-size 194
+oid sha256:e3ad474e2ab95c29bdc907c3c92a626dadf1555f1b9992e273d0e8df65e2cde2
+size 204
