272b539813fbdaa744c1e016a6415e7c0d8a65aa
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-17 15:26:55 -0400

    Added better trimming in angular_average Former-commit-id: 184913a94baf8d42d2838e06bc15e4db40c1bc4a [formerly 415792c65193186c60b1b1017ecf095b66922626] Former-commit-id: e26bd179f6f9a52da29a6e58c84a8334d08385d8

--- skued/image/tests/test_powder.py
+++ skued/image/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ee0b0c334c0b379f18cbc0741fee57f74a2b6aa9b67749f2c63b81f85180fb6
-size 4329
+oid sha256:5d14991e2ee85e77bccbee70af51ffdf3504dfa49f9e1d363f923969d9660122
+size 5706
