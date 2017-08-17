5adc6e681462b6be7d5d824a614fa1cf626ee50b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-17 15:26:55 -0400

    Added better trimming in angular_average Former-commit-id: 415792c65193186c60b1b1017ecf095b66922626

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9956ba8b99f362320bbceebf4d88fab702a07532f4cc48852e9bed36304d879c
-size 3907
+oid sha256:87b284200051974e59e1d5a3c1f8910afa6f49043332ac794fff0621124f282f
+size 4141
