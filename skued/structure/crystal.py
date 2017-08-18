28268cc3f66351e8d8f2e1ea84ffacb9a29f4e4c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-18 16:42:37 -0400

    FIX: error

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf9b485232b606f201aa1d95039636b5cbd54a44be9b04a79acfeb2e749f815c
-size 18977
+oid sha256:27e7200009923445f713d7199d3f0d90c1d6557a35f40ff0e6b18054c6e03bbe
+size 19022
