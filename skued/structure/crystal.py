d8cacd26130be00153ad0b45c2d7aad490a1c138
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-18 17:01:57 -0400

    Removed inelegant spglib_cell() method

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27e7200009923445f713d7199d3f0d90c1d6557a35f40ff0e6b18054c6e03bbe
-size 19022
+oid sha256:548221520e8058372fe8aaa2ae1281de2e231d9cdf52ccbfaf879a4c9f754be8
+size 19058
