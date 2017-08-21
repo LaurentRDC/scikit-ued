23e9c0576a3ced3de8475df2b94c5f52849c525d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 15:51:25 -0400

    angular_average renamed Former-commit-id: 402e94ee32f5bb39c9e413fe9940b39e94a77376 [formerly d12dbcb0d7e8f5911deafe201d0f16fa3e451af3] [formerly 701a6233023bbed1ae4b14e5f16d3bd01f523642 [formerly 3057e391f089b687564207763d2f3ac2f25860fc]] Former-commit-id: 1d1444edbaa5f85d318359d6fc0e263e24573db8 [formerly e5d94736f3a4aa4ca235822ae608bf5ef19ccc41] Former-commit-id: 2d12198b11f1017e175c52923b5b23f17490e81f

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb9a0b0da292488ddfe9cb4b34ed08206c280ab81f40c894db7a724c717e3fb
-size 250
+oid sha256:7531a51181206ae7e1d880305d56a7ad59cf80cd54e81a490f49b35ccae83646
+size 269
