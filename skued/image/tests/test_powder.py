bc58530a090a3fe0986b63a3919e8975893eaf0f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 16:34:32 -0400

    FIX: zero-trimming in azimuthal_average Former-commit-id: eb92df222df8914b912206450e1af1a97dedc3eb

--- skued/image/tests/test_powder.py
+++ skued/image/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9fed2013d083a090328efe80218a5b2ca2cebd0f36d32a7e7c8664882f53a99
-size 5602
+oid sha256:446e5776b59c5090e9bcb4152b720ce390f2b822c99c5a0a79c3a36cc5c4521a
+size 5603
