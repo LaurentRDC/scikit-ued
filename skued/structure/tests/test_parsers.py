d2f14daecfb0c9b90fe5863ef6721db77a511ade
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 13:18:58 -0400

    FIX: C.cif Former-commit-id: 73136a839a6c487eb89b22564dde56cf5aac6220

--- skued/structure/tests/test_parsers.py
+++ skued/structure/tests/test_parsers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06f077160958bffeff14b1034aca1ac72e097af9f1ebffc38b5cb94e4e6ea5b0
-size 4342
+oid sha256:69cf3958e39ed774c684c42d72a803287eb3cc61826b8e429a49403492252981
+size 4345
