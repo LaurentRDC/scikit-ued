38429ee49aac6fb77e039625e28b0c76dae81dc3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 15:51:25 -0400

    angular_average renamed Former-commit-id: 3fd23e84c76661af71511bb90d9ea595a69caa6a

--- skued/image/tests/test_powder.py
+++ skued/image/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1e2f7db255433c9e9b052e7cddc275b3cb135399f9ce2f17b883b03a2918f74
-size 5572
+oid sha256:e9fed2013d083a090328efe80218a5b2ca2cebd0f36d32a7e7c8664882f53a99
+size 5602
