531329c4daf90ad05b94ca570f622f043ebf45c6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 16:34:32 -0400

    FIX: zero-trimming in azimuthal_average

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebb7ffdc97c55fcab5cbead0cd4d23edfe05b77fe4560fb6a954866e8ae4b96e
-size 4164
+oid sha256:f2fdadd6feae33587ab42dd3308203bae6ac306471b2e3b5d66d7a31c5066901
+size 4202
