cdce8bcafb41e3782ed5e005de8b2c97760a23cd
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-21 15:51:25 -0400

    angular_average renamed Former-commit-id: 3057e391f089b687564207763d2f3ac2f25860fc

--- skued/array_utils.py
+++ skued/array_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8c91938bfe56216c4d73e72bb36ae7ba579319636a5af1e8616005c1a8e62dc
-size 1899
+oid sha256:e6e961e2e6deff4426b0612c62c86b7adc46da04947c2be4fecafe721c24f4e8
+size 2563
