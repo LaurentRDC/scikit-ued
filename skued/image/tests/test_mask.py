67d0dbcaae70808c7cfb3a13c489d728157d584c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-23 13:39:18 -0400

    Added mask_image function Former-commit-id: 9d518838560f37f1679ead42c6d99f11dce0fae8

--- skued/image/tests/test_mask.py
+++ skued/image/tests/test_mask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f763c92ba74afd8d8016d43eaf94d07084260ba937d778cae0ffc9109f25f9e8
-size 2357
+oid sha256:0ffcb624ab6cfb213e2040fee9a8ab0f1ab6c5508dc29cd324ed0856269068c7
+size 2920
