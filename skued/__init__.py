fc71fff63a7a9f0930127a7d3107caff13125ed3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-23 13:39:18 -0400

    Added mask_image function

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc9000ade82d64bda1af22300408ef73c98fa026620dbd138faabb0fb42e77d1
-size 1067
+oid sha256:37f070af4939b5e47c509f811c94135a0f402e2585a7aac0c9d3ac0ef31a4ce5
+size 1080
