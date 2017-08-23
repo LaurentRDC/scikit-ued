40a8187b04044bbc7f60bc08de5debde202d3aac
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-23 12:39:27 -0400

    Added skued.image.mask module Former-commit-id: 5cbc7c6b5c30338327db49d4515ab31453c3d1b2

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7531a51181206ae7e1d880305d56a7ad59cf80cd54e81a490f49b35ccae83646
-size 269
+oid sha256:71e8089a6802ecf4c850ffaa41adde4c86663c75f822efe3ef5dea36de0f65ba
+size 309
