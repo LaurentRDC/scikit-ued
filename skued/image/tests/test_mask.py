4670f6e39267967457e7c241eec45ef4957c97a3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-23 13:27:37 -0400

    Added combine_masks routine Former-commit-id: 274fc5269df0ff40b99771ba00f6b439c9719c46 [formerly 3145827e8aaa3610553f84af3c0aa8cb4c9eab61] Former-commit-id: 21d1adebc057d3740d24e0ae5942065fcb1684f0

--- skued/image/tests/test_mask.py
+++ skued/image/tests/test_mask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:def6c18dd91eea06603356e52626bace73bc86c5aa90e28b49804caf624034fb
-size 1860
+oid sha256:f763c92ba74afd8d8016d43eaf94d07084260ba937d778cae0ffc9109f25f9e8
+size 2357
