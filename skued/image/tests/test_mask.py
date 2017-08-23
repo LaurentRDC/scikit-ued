725a68abdbc3fd975dcbe4e25f7bf015e079eff7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-23 12:52:47 -0400

    Added pixel lower bound Former-commit-id: 1eaa1452d66ee9022877d3de93afe253ba2c96fa [formerly 05bc01329a1496ff7bc0353572c5d8d0fa462463] Former-commit-id: 9396ccc2f6ae24b5a4eff6442c336d075886efcf

--- skued/image/tests/test_mask.py
+++ skued/image/tests/test_mask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:744806eeb0427d8648c427ebde9fc45fbdafd8bb3cf347908e9e6fa0e0d13e0f
-size 1463
+oid sha256:def6c18dd91eea06603356e52626bace73bc86c5aa90e28b49804caf624034fb
+size 1860
