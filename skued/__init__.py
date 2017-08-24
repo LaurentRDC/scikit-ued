46d77b425ee7c41532770fdeaedc300106492ff9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-24 10:10:56 -0400

    Renamed quantities module to more appropriate eproperties Former-commit-id: 83f5b6ebde1c1cc263cd93e9165fd397e58f711e [formerly d6ccf7812dc10c93ea2aaaa8d3200969b7518016] Former-commit-id: f09cda481823f89e8f823e4f572532751b03b38d

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:976e66ea5a3e2a08b800d4b2a547c482941deba302f18da40fed8ea0f4e32ca6
-size 1092
+oid sha256:069fa09dd3e1a3a693738b13a288e0e2a7a46bd9de943ee74c9029832faea693
+size 1093
