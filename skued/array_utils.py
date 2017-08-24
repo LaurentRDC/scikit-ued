812d6c1b1d2445384646949b0748dbda6c002127
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-24 10:08:53 -0400

    Added plane mesh routine Former-commit-id: b46bf016f0d908af8df9306224ea99b361fdce0c

--- skued/array_utils.py
+++ skued/array_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6e961e2e6deff4426b0612c62c86b7adc46da04947c2be4fecafe721c24f4e8
-size 2563
+oid sha256:6eaefeacb94a5891847b0cfd4a8dd62387162c707fe5e8839ba17d9191b3886c
+size 3771
