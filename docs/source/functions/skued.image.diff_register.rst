183f076a8d75cf321c77f5d577df89e489d1b45f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-25 09:46:28 -0400

    Added SNR routines Former-commit-id: 874ed92b9d60c1cf35813335e3038fd5f7d35963 [formerly 6f776d683b2b5ae4a01d994bf67383ffc22caaac] Former-commit-id: 89be167e9d78bb7b8f66ad4a388e61a1097b467a

--- docs/source/functions/skued.image.diff_register.rst
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:356ed4e6839c377124d7cf54912c32a7ee750d317631713e9ad2d60839ecafe4
-size 122
--- skued/image/tests/test_mask.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0ffcb624ab6cfb213e2040fee9a8ab0f1ab6c5508dc29cd324ed0856269068c7
-size 2920
