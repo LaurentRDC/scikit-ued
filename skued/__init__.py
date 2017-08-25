f1b45f67146a92a252738a421c2b8502beb0f0ec
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-25 09:46:28 -0400

    Added SNR routines

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:069fa09dd3e1a3a693738b13a288e0e2a7a46bd9de943ee74c9029832faea693
-size 1093
+oid sha256:748c73b26e804ba12d2507435c4ec97687a8fc1d5cfbc6976fe7a3f614252e24
+size 1120
