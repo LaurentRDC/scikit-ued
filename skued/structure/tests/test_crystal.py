c86d2ae96c3554a099d82d7a892ee9f77885a445
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-29 09:57:19 -0400

    Moved structure-factor calculuations to simulation subpackage Former-commit-id: 516d12cdef4256ab768263a8a87b876021747411

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9276e2874862c89a80e409ee70df674adbd0be5e3af691122cbbbb00faec4e46
-size 6673
+oid sha256:4cc77d74a63bedba901b8dd27998cbb3e9ed243617934737b58343cd55e40226
+size 5591
