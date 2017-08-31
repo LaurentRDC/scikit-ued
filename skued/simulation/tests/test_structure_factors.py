ae3b7360711a2a3eaefbc745eb96c3765ef65aed
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-31 13:29:39 -0400

    Renamed electron_form_factor to more appropriate affe Former-commit-id: 42ed47a9b3b615bb26fa7aa706127c6a7ed2d1b2

--- skued/simulation/tests/test_structure_factors.py
+++ skued/simulation/tests/test_structure_factors.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5e8cda0eacf618c4779fb394bdf21154913930b3d9e3ffd2ef3b6d2b0b4344
-size 2319
+oid sha256:9a70a468fb51823692807b7a88b3b09b0d32aae03c427e2457f0cf5ac8793d4e
+size 2271
