1965500bf74c5b42643173ba4f5bda7c3667c572
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-07 10:29:08 -0400

    FIX: typo in tests

--- skued/simulation/tests/test_structure_factors.py
+++ skued/simulation/tests/test_structure_factors.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c801185f183e23f444b052fb81fd6c8578b0a5bd62bd1bab23ed2b256512546a
-size 2246
+oid sha256:7ea0e1261d40388171c6f2d3ef621d892541e911e78d2ec60fcc2472b41d19a0
+size 2237
