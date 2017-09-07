851a2f6cf81e2948f0639ce04b351aa1dbea0440
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-07 12:17:27 -0400

    Added normalization to structure factor calculation

--- skued/simulation/structure_factors.py
+++ skued/simulation/structure_factors.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a138bf2609160d39744faee3930908ec511dbdaa6bd38cf67a8733f400dbac1b
-size 4781
+oid sha256:dcdac851b5103bc88b522178a2dcc0f0d5718058f114543fa4760daaac099a3b
+size 5097
