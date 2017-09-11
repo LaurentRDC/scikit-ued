edbcc1966ba1b5302b87b631375c083ac02dfb9d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-11 16:15:27 -0400

    Added conversion between cartesian and spherical coordinates Former-commit-id: aa21cabb2758aeae405c88b233feb94839e4bb2a [formerly 20798d2e0530d664a4907abbdb62f614f0fda1c4] [formerly 864a67638d88c75e83cd2006bc2862f9c87fe9ec [formerly 6ef370eb80f4a25ad42ada14d7322d84458409b2]] Former-commit-id: 185041f3b31998d33527c57d3604e9d49b7d1423 [formerly 6281a3ca7a726af692b59309649e5517d059509e] Former-commit-id: 6c0f3a56817020475dfd91295f9883e6e1743266

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3115abeb22a0fdd47dc2687f8faf9055b4a4bc5bc54691d323ef8424fcc1f8af
-size 1165
+oid sha256:96eb6f9823786ef72f82e1385f1bec0e83d0d47a0123b92fbc32f80924edc9ae
+size 1253
