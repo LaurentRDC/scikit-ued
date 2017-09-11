e6e60cec7c58f4fd092cdfea880db0e5badb8fdc
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-11 15:53:24 -0400

    DOC: fixes in affe

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05d2470e276ebea67519cdcfdc2c73d3ebe0518853cae1130636e6accfd7800b
-size 1159
+oid sha256:3115abeb22a0fdd47dc2687f8faf9055b4a4bc5bc54691d323ef8424fcc1f8af
+size 1165
