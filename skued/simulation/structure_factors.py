e91b48656d674595fcad8a6517b11655777cc208
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-11 15:47:05 -0400

    FIX: affe accepts atomic number as INT Former-commit-id: eb0a9e4c3fcf9816dc6c54b81ff81f4a65adbfb0 [formerly f5e9d38bf458de5aa775740e29d21da7817b58b3] Former-commit-id: 95c930bc44265ca344eaf4f183c77847e30cbce4

--- skued/simulation/structure_factors.py
+++ skued/simulation/structure_factors.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcdac851b5103bc88b522178a2dcc0f0d5718058f114543fa4760daaac099a3b
-size 5097
+oid sha256:c08c55132ecf854289582022bdc2894860a9b1879fa49205805b5a3790a812eb
+size 5227
