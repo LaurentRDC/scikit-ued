9415d279a2ff456ba0eb59683fc3baf697383bd4
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-19 17:22:51 -0400

    Added array trimming by percentile inspired by scipy.stats.trim1, I have added routines (trimr and triml) that trim array values below (triml) or above (trimr) a percentile. Useful to get rid of unphysical noise Former-commit-id: 700702b37c8b5e0695b9985285cb0bc6aa3ef317

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96eb6f9823786ef72f82e1385f1bec0e83d0d47a0123b92fbc32f80924edc9ae
-size 1253
+oid sha256:0c41263178a024ea94e5e2df641889a0cc66d386b7fbe020cfdbcfcd6e744660
+size 1267
