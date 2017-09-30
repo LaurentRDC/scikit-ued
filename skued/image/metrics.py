3a0a788dffeaeed673e0442b3acd3e3d4fb896c3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-09-30 14:01:07 -0400

    FIX: baseline_dt could returne a baseline that was negative At each iteration, the baseline computed by baseline_dt and baseline_dwt is forced to be nonnegative. Former-commit-id: 18e33e4ce72202af239ef63743c375406c766b5f

--- skued/image/metrics.py
+++ skued/image/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:730e30e7ad3af372569ba67d90650af8d15637177ef4a1b1baf9d43f1e9ebe78
-size 8055
+oid sha256:f214c5a65eff6df628cdf8784155f28b846992343ec6b6397ac2685fa2329afd
+size 7983
