f2a983e87d52bde0886b616c3701f812775e3763
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-11-23 09:53:59 -0500

    Moved dataset module to iris

--- skued/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Dataset interactions """
-
-from .meta import ExperimentalParameter, MetaRawDataset
-from .raw import AbstractRawDataset
-from .mcgill import McGillRawDataset
-
-RAW_DATASET_CLASSES = { McGillRawDataset }
