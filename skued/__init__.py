d08ebe48bac4baf43176e3e651f58d78f43429b2
Laurent P. René de Cotret <LaurentRDC@users.noreply.github.com>
2017-11-23 13:59:36 -0500

    0.4.10 update (#29) * Bump requirements for npstreams * DOC: lorentzian * Added electron velocity function * Added an interface to raw UED data * Added some tests and documentation on AbstractRawDataset * Tweaks * Moved dataset module to iris * Removed superfluous dependencies * Updated README * Version bump to 0.4.10 Former-commit-id: 4af0d3eab414b5b76fd70507d7b7f662b9650b8e [formerly 254c5327d3b4559def401405955eba3b125e0db5] Former-commit-id: d37efefe5424025e79613d481b9adf2228fa9102

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c41263178a024ea94e5e2df641889a0cc66d386b7fbe020cfdbcfcd6e744660
-size 1267
+oid sha256:06d8cc9c9714e9ee89d02bd547bef26140d8963202164a020d87375d3e97ba64
+size 1333
