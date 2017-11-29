fa95de41683bfccbe8f2cbe59eb9f0f86c94173b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-11-29 10:44:12 -0500

    Added a test for shift_image type promotion Former-commit-id: e2158c0be6d7566d799d8211e4fb021194eeba24

--- skued/image/tests/test_alignment.py
+++ skued/image/tests/test_alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdd6e97e123a7ec49bb3d017fd4e2a453d6aa9741a8f0a62fefaa39d65505974
-size 6086
+oid sha256:3ea01ac9042f20ed0512ac0e106f97f988da3448fc3561cf7eafb4973ca45d9c
+size 6123
