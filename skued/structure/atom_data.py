4d89ed09f98b2daff247c211dc09931310b44291
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-03 15:24:56 -0500

    Import cleanup for structure and image subpackage Former-commit-id: 51365063ce6520422bf2ac11045db6c99aaf535f

--- skued/structure/atom_data.py
+++ skued/structure/atom_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6c612df82733419a2c7eba6b18ce1fe5b83f7ec965fb938c128bb4730bad5c3
-size 8234
+oid sha256:de786afe6f6e7c5b9ada25246db8f9177ccb927fafeb3a1726978bed924265d7
+size 8215
