32209523db3925b2a78bb56632d9410e34ee24ae
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-04 09:30:26 -0500

    Updated nfold to use npstreams Also removed deprecated nfold_symmetry Former-commit-id: 38e919a85a49f3ed38012f2444f530e617012d3e

--- skued/image/symmetry.py
+++ skued/image/symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7372d70f3c0d0dad12efbcd6db6164c126680c30fd140aaf3fad53edeced2b4b
-size 2336
+oid sha256:74690b446e26985e7855800e342251480e40a9a176498b94cbfe632eeac18a10
+size 2154
