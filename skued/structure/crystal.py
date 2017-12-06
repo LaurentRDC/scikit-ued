d5df70364345660729ad951846458fdec4166541
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-06 16:17:56 -0500

    Made Crystal instances picklable and hashable

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e53e4b45ae843187b7a44cc686054647eb7517f3d494b0cd06a3827535fcf8b6
-size 12418
+oid sha256:2892f3ef4c628d7a0642ce5c71e542cc4e26b32cde6d453e79de261a6a83b40a
+size 12831
--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:882f553eae945580bf4fd3513d7925ef0913400310fef8795d5f4e067703bad2
-size 5972
+oid sha256:2b9c95a4bd5b38c0578b48ef8af1a1c20feffc6f11f0b01359d8d121900a37eb
+size 6703
