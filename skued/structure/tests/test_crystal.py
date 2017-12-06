46285ca6eb0a4fb31b8bb159b7cf1988aa1e7f64
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-06 16:30:33 -0500

    Added containership to Crystal

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b9c95a4bd5b38c0578b48ef8af1a1c20feffc6f11f0b01359d8d121900a37eb
-size 6703
+oid sha256:3631110b0894cfe7cb1810dc7b118bad6ee27b3f920e8dbf3b1cef82322615eb
+size 6887
