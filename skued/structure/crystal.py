10e338d40ca6d8753d4b9bccf3db3c5ad1078004
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-06 16:41:50 -0500

    Added a test for correct creation of a cache folder in Crystal.from_cod Former-commit-id: fa5bb56c95f79327bf8b957ef1de4127c56d73f3 [formerly 798f16aff4e947bc0a331ee36cf4ff0392dbba9b] Former-commit-id: 355068174df09e35d43e5716b8f5e9e977aaef20

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe10533ce53807b95b8f189be35ad96f271764c6c9d0e564cd130272d8cd5e2
-size 13093
+oid sha256:806864df6c04125adf5d30f901b2737841c5fa7bc7d3e5a36a1f5afc450f75a7
+size 13111
--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3631110b0894cfe7cb1810dc7b118bad6ee27b3f920e8dbf3b1cef82322615eb
-size 6887
+oid sha256:cb9c4b5408897e37dd33937ea9b3da76e38caf88ae0d662aa4d7d01f468395d2
+size 7328
