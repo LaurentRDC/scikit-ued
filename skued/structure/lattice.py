ae22336b1c53b71652f62065768a6ff8c17dd757
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-06 16:17:56 -0500

    Made Crystal instances picklable and hashable Former-commit-id: a37fc89ec16062f7a59743c7ea46735527f92128

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab65190fb720f9a700daa4b9dc4d0f3638a3d98a940f74ab5d7c6e3246259fd0
-size 6026
+oid sha256:c51397fce5795296d7fa70caafcf3538d71a3b7b3c6bf972322be3b7bbdac6c2
+size 6027
