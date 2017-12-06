badf5328181f8482e956180e0ea0ddf29febf52d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-06 16:45:03 -0500

    FIX: catching errors in Crystal.spacegroup_info

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:806864df6c04125adf5d30f901b2737841c5fa7bc7d3e5a36a1f5afc450f75a7
-size 13111
+oid sha256:c03babc6affe6cd61995bde18821cf2c91692ace86a295d148c01e93ce67336e
+size 13140
