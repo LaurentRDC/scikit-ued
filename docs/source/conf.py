d72c214036195068d97f3f6715f13de5212534ec
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-07 12:22:52 -0500

    DOC: separated documentation of skued.structure Former-commit-id: 22537e2f7d6dd7748dea31f6af4c6986544947da

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecbe94bce53d49822d5f8a829b9ab566436b13e3a1ef2ef959d8be22a63aaa86
-size 4415
+oid sha256:8bf2d055299a307db3e3c94b00c819a77253689bea434d552628d5ff7373c6a3
+size 4442
