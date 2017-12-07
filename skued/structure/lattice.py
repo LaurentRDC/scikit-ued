76f2785c15edf284b3316529a1a0bf6614c0ef97
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-07 10:13:29 -0500

    Added mesh generation methods to Lattice class

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c51397fce5795296d7fa70caafcf3538d71a3b7b3c6bf972322be3b7bbdac6c2
-size 6027
+oid sha256:fed28c01ea6a7c93a6c5de10a604485b04f54fd2226a2dd3d07038a9eeaeb3cf
+size 8441
