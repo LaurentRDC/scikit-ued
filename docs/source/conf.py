6b6e02cbd76d0036d803d51304fd216c20d416a6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-07 19:56:20 -0500

    Added a base class for Crystal This base class AtomicStructure abstracts away the iteration over Atoms and substructures. This will be useful when substructure are defined in Crystal instances, like atom chains, atom planes, etc. Former-commit-id: 623dfbff0ceedd11f47654191c60e0a4ebd902fc [formerly 9711af6266acaeea51c19362a6cd3ebe93497662] Former-commit-id: 9871b1c527d30dc27733653389291129f11608a0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bf2d055299a307db3e3c94b00c819a77253689bea434d552628d5ff7373c6a3
-size 4442
+oid sha256:e33e8c53c8316151fac2085507bc2f0208d913034168fb2a3445f05dbd715dda
+size 4429
