3d2956350ced938a2b61045290f740c4578440ed
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-07 19:56:20 -0500

    Added a base class for Crystal This base class AtomicStructure abstracts away the iteration over Atoms and substructures. This will be useful when substructure are defined in Crystal instances, like atom chains, atom planes, etc.

--- skued/structure/tests/test_atom.py
+++ skued/structure/tests/test_atom.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b84237afd4a47ef8b88c0dfd398944468ba9037ac6832a3973ef0dfa761049
-size 2328
+oid sha256:da94a93606e064380592cc5fe8d9c9b92cadfbf3a844b91d2f6988abbcc39cee
+size 2375
