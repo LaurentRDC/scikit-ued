25dcb8ce780497feb583e81ecaa5ebdb5a582e19
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-11 19:22:12 -0500

    Added a lattice_system function and attribute to the Lattice class This function allows to determine one of the seven lattice systems Former-commit-id: 91358790fcaa24469aa4b7a04cb560623afe004a [formerly a497ceecb18379b002b0bcd65f92693a5bbd4977] Former-commit-id: 32939d2fd8a76ba3c10936fc3aa5f2462a60f1f8

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc433ef56404ec1422fc77e7dec0d8fc093f924dd77bb7b91eb98d9d8a8dfd2b
-size 8694
+oid sha256:e432741e0a4cc3c08a04e7bc160a048a70efbc214e4b9867bfe0034a12e674c0
+size 11945
