cab50ec60bed4ecac1cb8edfa7f9f36e7a1ad1ca
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-12 08:57:29 -0500

    Removed deprecated angular_average Former-commit-id: b942bfad78e594a44b5b52c7d2cc24acbba54907

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c836c64471be8799a3fc6d6135a74372b7c9b9e773d13b26a97c166d39bb6cd
-size 364
+oid sha256:aded2bfd473764c9c1eb40cba55d4606bc24f03180081dd9eef69a13875110a3
+size 347
