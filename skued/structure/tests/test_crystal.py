211faf90d9d84f38a042c5ff075e44ea2846877d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-12 08:40:05 -0500

    Removed extra tests Since Crystal is derived from AtomicStructure now, some tests were redundant. Former-commit-id: b05ba9212fe22745d3ff31bdf3a5a99e74005c9d [formerly bb5db16e5593cab353623e80fa93de2a1c7483a1] Former-commit-id: d09e3a9e25eb9df8bab065d710049847d41c5e32

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb9c4b5408897e37dd33937ea9b3da76e38caf88ae0d662aa4d7d01f468395d2
-size 7328
+oid sha256:77c1f8d69e6d3f0848d17c68b063c6267e09c9b76c32d5a83ebc3c730f03e45e
+size 6115
