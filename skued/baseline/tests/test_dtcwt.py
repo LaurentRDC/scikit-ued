45393022205b4ac0820dbb0f39278cf192473002
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-12 08:49:17 -0500

    Added a chjeck on dtcwt An exception is now raised when the data long the transform direction is not even. Former-commit-id: 92743443f85c4cdf9bd39c46e17d0357f82e56fd [formerly aed3b0c8e165d32cf25cf28d4234ec3563377c02] [formerly fd32f3cce924868c7a721c9eca25b1865a262b39 [formerly 8c48472dab2478fca19e212edee032322e577ea6]] Former-commit-id: 5bf9f4763c1d53e09fd12311c073dc875d553e4d [formerly 6dd9f735ff7198aa1f9d12864d619c196d8dac10] Former-commit-id: 6c6761d423ebfa167b4339b62a5e9f99e12f36a6

--- skued/baseline/tests/test_dtcwt.py
+++ skued/baseline/tests/test_dtcwt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a0142f80a05a4303f027b244f984baf4fd81c8bb3638c81099f568b7dda00e2
-size 3548
+oid sha256:cfb95de219f72cefeffaeb7e6eb19ee647696e56cb5911699bf843ea4df68e94
+size 4240
