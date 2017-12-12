b54dbc48bdd38e5fccc42fc16fc08b71c0f3e4a5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-12 08:57:29 -0500

    Removed deprecated angular_average Former-commit-id: a173ccf7bbe3a7aef11f6f5b78d132eaf293c57e [formerly b942bfad78e594a44b5b52c7d2cc24acbba54907] Former-commit-id: aa014329e2d78ff5470fa937c61d6710ed40a9d1

--- skued/image/symmetry.py
+++ skued/image/symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74690b446e26985e7855800e342251480e40a9a176498b94cbfe632eeac18a10
-size 2154
+oid sha256:84996ccd6a47aca1976ef007622755f15b53787dbcc71c6b0e2b34fafde26dfd
+size 2148
