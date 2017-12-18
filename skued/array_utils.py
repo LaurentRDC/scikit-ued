eb3fb06a11f55e77dd85c3522535b0b4213c3783
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-18 12:56:05 -0500

    Small performance tweaks in baseline package

--- skued/array_utils.py
+++ skued/array_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd1990f24949f849cc5c32ad71c48ce1edcc1227345c688eeeaf3260b163fd8
-size 4925
+oid sha256:db40e96ce1d5b79c9a804595f3bcedd6cab65ace235a40b3d5f436e15251984f
+size 5345
