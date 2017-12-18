9072df4566cd69923b8bf2f0b42ddb1670874612
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-12-18 11:52:24 -0500

    Added Nick Kingsbury's QShiftGen Publically -available package based on Kinsbury 2003 Former-commit-id: f8453fc32be033c0307a378996eb87316f3d7503 [formerly 1f51aac7dcf1dfab07e93fc6ebbbcdb10de4aece] Former-commit-id: 45f5ea320b9f60ad057e2a61850cddbd800498ab

---
+++ external/qshiftgen/deconv_sym.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f60afd689d8c952400c0fbf64948dd7f0bf2564ab546b893fba2a2d2e7b4de18
+size 565
---
+++ external/qshiftgen/qshiftgen.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2f26652fddbabbb0abbecf23fa8a83b95c5f40ae1a283a097f46922052b4a1b
+size 4808
