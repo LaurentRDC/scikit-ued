db7d9867978cae37ea956de1ef75941f7522f55b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-05 13:41:57 -0500

    Simplification of calculation of nfft parameters

--- skued/time_series/nfft_routines.py
+++ skued/time_series/nfft_routines.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dff4ac45b09a21bf3afa58a933ca2cdf4c3e0bf94f229825cc1736513fff94a
-size 3333
+oid sha256:32788fd0ec4d6cfa02822579e7424e79b29a80d6cd2969059a1d4d204553a707
+size 2812
