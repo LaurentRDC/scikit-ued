be9da754e13cb7a5ecd2bb0b3c5978116063d20d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-05 09:12:35 -0500

    Added support in time_shift for convolution method

--- skued/time_series/time_zero.py
+++ skued/time_series/time_zero.py
@@ -5,10 +5,8 @@ from functools import partial
 import numpy as np
 from scipy.signal import correlate
 
-from npstreams import array_stream, peek
 
-
-def time_shift(trace, reference):
+def time_shift(trace, reference, method = 'auto'):
     """ 
     Measure the time shift between a time trace and a reference trace
     by cross correlation.
@@ -19,12 +17,14 @@ def time_shift(trace, reference):
         Time trace. Must be the same lengths as ``reference``.
     reference : array-like
         Reference trace.
+    method : str {'auto', 'fft', 'direct'}, optional
+        A string indicating which method to use to calculate the correlation.
     
     Returns
     -------
-    shift : int
+    shift : int or float
         Index shift between the two traces, with respect to
-        the ``reference`` trace. 
+        the ``reference`` trace. Note that the shift can be fractional.
     
     Raises
     ------
@@ -33,20 +33,20 @@ def time_shift(trace, reference):
     See Also
     --------
     time_shifts : measure time-shift between multiple traces and a reference.
+    scipy.signal.choose_conv_method : contains more documentation on ``method``
     """
     trace, reference = np.atleast_1d(trace, reference)
     if trace.shape != reference.shape:
         raise ValueError('Time trace and reference trace are expected to have the same shape, be received \
                          a time-trace of shape {} and a reference trace of shape {}'.format(trace.shape, reference.shape))
 
-    xcorr = correlate(trace, reference, mode = 'same')
+    xcorr = correlate(trace, reference, mode = 'same', method = method)
 
     # Generalize to the average of multiple maxima
     maxima = np.transpose(np.nonzero(xcorr == xcorr.max())) 
-    return round(np.mean(maxima) - int(xcorr.shape[0]/2))
+    return np.mean(maxima) - int(xcorr.shape[0]/2)
 
-@array_stream
-def time_shifts(traces, reference = None):
+def time_shifts(traces, reference = None, method = 'auto'):
     """
     Measure the time shifts between time traces and a reference.
 
@@ -59,11 +59,13 @@ def time_shifts(traces, reference = None):
         If provided, the time-zero shift between the traces in ``traces`` will be measured
         with respect to ``reference``. Otherwise, the first trace in ``traces`` will be used 
         as a reference.
+    method : str {'auto', 'fft', 'direct'}, optional
+        A string indicating which method to use to calculate the correlation.
     
     Returns
     -------
-    shifts : `~numpy.ndarray`, ndim 1, dtype int
-        Time shifts as indices.
+    shifts : `~numpy.ndarray`, ndim 1, dtype float
+        Time shifts as indices (possibly fractional).
     
     Raises
     ------
@@ -73,11 +75,14 @@ def time_shifts(traces, reference = None):
     --------
     time_shift : measure time-shift between a single trace and a reference.
     """
+    # TODO: if ``traces`` has a length, we can allocate the shifts array
     traces = iter(traces)
 
     if reference is None:
         reference = next(traces)
     reference = np.atleast_1d(reference)
 
-    shifts = map(partial(time_shift, reference = reference), traces)
-    return np.fromiter(shifts, dtype = np.int)
+    kwargs = {'reference': reference, 'method': method}
+
+    shifts = map(partial(time_shift, **kwargs), traces)
+    return np.fromiter(shifts, dtype = np.float)
