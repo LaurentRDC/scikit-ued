375f79799b88c46fba908eff07e0a0401ebc23ba
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-05 09:31:03 -0500

    Ensured that time_shifts array is always the same shape Former-commit-id: 40ef14d4f168b58ab16ba9a870d171b3c69a40f2

--- skued/time_series/time_zero.py
+++ skued/time_series/time_zero.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:959ccbff882cdb2ad8f1bb849dae8aa216af92462c39d9ee06d61b04ab34495b
-size 3078
+oid sha256:0bba4dba94b783ada0ce2b9025e03e63a0ea6af8d9e8a283ee6efccb7597e883
+size 3301
