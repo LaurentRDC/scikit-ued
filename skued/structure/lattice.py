3dc82828f90968777a87bbab41ba3b98256946a8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-06 16:08:09 -0500

    Added LatticeSystem Enum Former-commit-id: 6519e136e1a3b050ff47790421a1c9f04d13e823 [formerly 514f95809907823aa7c16448dea7fa83b3227d4a] Former-commit-id: 3eb7bcf175b1a5c2bf0d37690d54ffebcc2693bc

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:332dbb88ff330879f6d1e51aa5bb3981b6c7f9e7aad3c7373f80736603f187bf
-size 11924
+oid sha256:24f4e8a7e8c1870af0d2ab8362ea35ea0a27fdc08c1c1725de029065b58a86e9
+size 12290
