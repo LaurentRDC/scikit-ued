77d4d329d49ec7c643c7fa30e30127a3aa71fc75
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-06 20:29:37 -0500

    FIX: removed enum.auto import since it is new in Py3.6 Former-commit-id: c6a900e60347a2396bf111ee6fb7b467c451e1a6 [formerly ac671f46e5198a8ae85f2845157b5ca8b99dfe9d] Former-commit-id: fa4b9ed8e693f0793242c46b83fddcdf221ce5ae

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24f4e8a7e8c1870af0d2ab8362ea35ea0a27fdc08c1c1725de029065b58a86e9
-size 12290
+oid sha256:4fb9cdef3122c801991b6214f7a95dd0e07b08b0cfc3521adc62638c0ff0da40
+size 12285
