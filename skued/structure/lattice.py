6ca0a0375c2b6be9f80582dc5da923c487eeb3b5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-07 11:11:05 -0500

    Remove `lattice_vectors_from_parameters` function Functionality was redundant with Lattice class Former-commit-id: d699b83c463e771ca11b5ce8561097b3ead598b9

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fb9cdef3122c801991b6214f7a95dd0e07b08b0cfc3521adc62638c0ff0da40
-size 12285
+oid sha256:9e65bd135d983cadd4e2a300ea3496bb302b75b6d081bab296f62bfaabeab14f
+size 11892
