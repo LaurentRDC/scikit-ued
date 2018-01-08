2956971eae52c1a4b48654fdd8989091f82b5134
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-07 22:51:13 -0500

    Added general cross-correlation function xcorr

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aded2bfd473764c9c1eb40cba55d4606bc24f03180081dd9eef69a13875110a3
-size 347
+oid sha256:41bf17d9994810cb62085dfc5967c9ad16e9732cf9fd4f191eb672ce9179c490
+size 354
