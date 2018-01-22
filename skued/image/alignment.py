5e28d66daeb60e1501bf99195e9d46fd74d6ea31
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-22 08:47:52 -0500

    Added the possibility of not cropping in diff_register

--- skued/image/alignment.py
+++ skued/image/alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a891927bf440eb8ffabafbf351c20b488d76630e3477e83f9cf8abb929a7e529
-size 5757
+oid sha256:9160012e7dcca2f6504d193fac53537538b1799770d09b35b559f41b6f00d861
+size 6148
