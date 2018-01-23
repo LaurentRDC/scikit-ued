c9bb0e7a77f617e988b84a6ac6368f13cd81e25e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-23 18:11:00 -0500

    Added itrack_peak function for tracking peaks Former-commit-id: c13e623eedc49b8da0483ea44ff74d49d76d939d

--- skued/image/__init__.py
+++ skued/image/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41bf17d9994810cb62085dfc5967c9ad16e9732cf9fd4f191eb672ce9179c490
-size 354
+oid sha256:43f5961a165f17f6647466dce204ad854d665929c45ab939cf87a9f26612e614
+size 367
--- skued/image/alignment.py
+++ skued/image/alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4e3e8357dd8bd40634fa289989266094387746e6357a0a56d83fd39804f3382
-size 6500
+oid sha256:ba1d5f8cff8b1e5f2ced408ee1961ab89edcecafcd9a51e8c48e29666b1c528b
+size 7833
