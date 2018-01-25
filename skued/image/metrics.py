0bd866c60f7946139cea832c54e6f295c2a0a4b9
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-01-25 14:50:52 -0500

    Added Merlin Image Binary file support Former-commit-id: 91684270ff4d90a55c9f883e0e6688e3a39def82

--- skued/image/metrics.py
+++ skued/image/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f59fa7450ea2d9021996111ede5b3f050c25bfd7cf963f3ccf7cec82da27b4a7
-size 8000
+oid sha256:3ed1ed79bd00a9fca428205060319faaa2107ce0978b18f7daaa6ec7737f10d4
+size 7999
---
+++ skued/io/tests/test_merlin.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d71136e9352ed6487456a6f10bfb73f88240447014ead1009499710231ef6f20
+size 1632
