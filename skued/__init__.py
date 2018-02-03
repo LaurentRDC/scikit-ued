b720bc8cd364be07bd896ec72a97b6b6688a5475
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-03 13:33:29 -0500

    Added diffread and diffshow functions Former-commit-id: d819b374421b478d92782de490bd7bba153a464e [formerly 4684e5e140e20174aab6457be8d178c07f432a79] Former-commit-id: 98a6abc1fe3270ef76363a241abc3557b1f188a6

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cabf58f899b6d51bb448a183607818001eb1ec555dfac9dc515f814635390169
-size 1603
+oid sha256:e32c6ddec03fbdbebb572e63f98d247e141ec5a01f0d25a8d108039d645a68bd
+size 1623
--- skued/io/__init__.py
+++ skued/io/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1fe1adf769e759aaea4038e00372a58b740d58eb2694c39836a6830669eea45
-size 190
+oid sha256:e1318af4eb9592de3184e4333dc771342aa4250d5a80785986987a307ed6aa96
+size 225
