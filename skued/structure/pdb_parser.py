063e983cfef4bbf1d8ca9ddaff2d0a36029c1452
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-05 18:52:36 -0500

    Updated CIFParser to use AbstractContextManager Former-commit-id: 053020710203508fb9da2f88d9009b6dabf1424c

--- skued/structure/pdb_parser.py
+++ skued/structure/pdb_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab1326fe6b61b496a324b35bdd049fbcd521a15b7ca11750772aea05fc3a6459
-size 5656
+oid sha256:03391c3ef07dc01bf97fe6e3d3ced636e8a1e39bf37338e00f3854a2141b67fb
+size 5648
--- skued/structure/tests/test_parsers.py
+++ skued/structure/tests/test_parsers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47637e3b11d35390dac10d3d974e5405a97105712c95dc80dbd10899dee95268
+oid sha256:0424b9d9d2bd28d13bda3cba40bb281fd12ce92471149f2e0fbe865e6147102b
 size 3994
