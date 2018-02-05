d21e77e666db27bdd4b18995337287df2f9bb67b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-05 18:52:36 -0500

    Updated CIFParser to use AbstractContextManager

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:268d2317bb666e5c241cedd9d80aaba887d37964abde5ede5a2c945481a7c29f
-size 10324
+oid sha256:f6c3bda9554d3331c094a6254874e4d961584eb36278efa70374a31b32f48b45
+size 10357
