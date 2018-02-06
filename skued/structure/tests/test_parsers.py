f3203d5811973b23927ab5af22a0673a82866cc8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-05 19:16:46 -0500

    Made PDBParser into a context manager Former-commit-id: 128266f5244223e841334a2a4dae7ae19d3d86cd

--- skued/structure/tests/test_parsers.py
+++ skued/structure/tests/test_parsers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0424b9d9d2bd28d13bda3cba40bb281fd12ce92471149f2e0fbe865e6147102b
-size 3994
+oid sha256:801901bd373038823881bfcfa811cfabc3ff92c93d4721a287e007555b571d60
+size 4055
