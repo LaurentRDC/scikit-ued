b21a971b42462fb91cfd5c6dcf0572007d4e3f9c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-20 18:49:11 -0500

    FIX: collections.abc.Collection does not exist in py35 Former-commit-id: 5b2f39f9a68807be5e3f64d1801f959edf38cdeb

--- skued/structure/tests/test_base.py
+++ skued/structure/tests/test_base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2daf3b2da71d192ec9d5719ee55b191142e03c481f525653fdb8cd15c86f336
-size 2477
+oid sha256:29aba9b7cb205b4b77895a2da29c72080f9407ab32cebd4e5dde5c6a0a2755cd
+size 2461
