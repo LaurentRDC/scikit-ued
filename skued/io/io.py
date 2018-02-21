686bf7a280b96b97a3bb054333c07d31b9dfe475
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-02-21 10:34:58 -0500

    Added ability to diffshow arrays directly Former-commit-id: 1e5baa344dcf2d681ceed673f2c6d3f9c288d92e [formerly 5ef89c0d2ba2eb5e6f0d078c5e374edd8cb7c841] Former-commit-id: 8f35269fae75b2eb7c093567480d337e1197e60f

--- skued/io/io.py
+++ skued/io/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaad509ee210b4b2590f6fb607f8b1c084268029108b6f1b999a85f8bce829ab
-size 2105
+oid sha256:b6815e4ab431e28512bc201f2f694544ac9c947648f0eaa643952c8c4a493077
+size 2147
