ad7f4d5147e4e97743a11016cb0b0da08438ff72
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-07 08:47:57 -0500

    DOC: fixed typo in docstring of calibrate_scattvector Former-commit-id: 9f6cd7f26c4de79b4cdcbfc4573af9f748db5b1c

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf28c54fd2413c100cfb27dc1d5d3075aec873443bea19f3e478eeb9cb9fcad9
-size 6045
+oid sha256:8dba35c1689d0dd614291aea3ff41439207c730ea501deb6670967bd91508b7d
+size 6047
