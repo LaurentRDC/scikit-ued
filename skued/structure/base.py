c841a785f3f497084df87d00b45e2703500a6c20
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-09 16:04:06 -0500

    Added itersorted() method to AtomicStructure

--- skued/structure/base.py
+++ skued/structure/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a75699a9da8fdc7e6db2fe728a4a40160d48af44a2df5205614e5fa43e331f71
-size 3295
+oid sha256:36ba55fb4e1f8f13cef56d44d79804ddf5a842a8066bc446ec7944a88672bbe0
+size 3953
