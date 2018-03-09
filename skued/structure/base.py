7744f8f24b923ce8ca59d50debca7aa72b6a5e4a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-09 16:07:10 -0500

    Faster containership testing Former-commit-id: b72d17a381e2bfe0df0c3689c88524b497afb772

--- skued/structure/base.py
+++ skued/structure/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36ba55fb4e1f8f13cef56d44d79804ddf5a842a8066bc446ec7944a88672bbe0
-size 3953
+oid sha256:c70e6f1f7853acbbebeae40b50afd184f5c82f6508404a5fb40645dcdd1bd87e
+size 4002
