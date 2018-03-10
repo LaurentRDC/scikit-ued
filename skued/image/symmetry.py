76fea26f4c03aae59e456e39585dc0dfb3e6d5f0
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-10 14:16:18 -0500

    Better handling of edges in nfold Former-commit-id: f10b14bccd12560c4f7ad39ba982876ae0fb6fae

--- skued/image/symmetry.py
+++ skued/image/symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84996ccd6a47aca1976ef007622755f15b53787dbcc71c6b0e2b34fafde26dfd
-size 2148
+oid sha256:1e8f319c040747363daa7cc6833dcdc40f84800e686f7a7d86d025dd755106d5
+size 2273
