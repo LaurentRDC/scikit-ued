e2b08aa646f5205ea735d59a39020e3841efc3fc
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-13 16:50:18 -0400

    Tweaks Former-commit-id: 7d0d736d444fb40fb677b7e070deb53ab5053549

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:730da4dab9d7ad986f77b56c28eef30c84f9e4fb1715307d4b2b5fc11132b4cc
-size 13805
+oid sha256:65d4f43952b242aad27e21c7c7222481e2c3d4c537ff9d99ec2f3330d51f8e2e
+size 13713
