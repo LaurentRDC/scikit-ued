e7db28f108ed1cf35c0654aa6fe357f0a376e63b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-13 16:50:18 -0400

    Tweaks

--- skued/structure/base.py
+++ skued/structure/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f17afbfb72677b8a54056da8b3e704ae5738edb7b07e0d070cf326b61723a0f
-size 4002
+oid sha256:fb6e2160ce639e62469752dc7815a778bd7fa0c5de31dc2b05be621fb65d0a9d
+size 4495
