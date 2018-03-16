870721bcb3a8463a2a51572a2372898014b04cc7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-16 16:55:28 -0400

    Added calq, powder_calq Former-commit-id: 4abf517b9ed43832594a6bda34d8f44c6eb866c8 [formerly 2776f6dede6bba473cfd1eeb3245f273a64f9714] Former-commit-id: b01537a59f43748b70229e6519450141fa920c8a

--- skued/image/powder.py
+++ skued/image/powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dba35c1689d0dd614291aea3ff41439207c730ea501deb6670967bd91508b7d
-size 6047
+oid sha256:9e09f79ac14080a592076a0a02f207d9ea3440eebf19665ab1f1cea8a1d9b110
+size 4354
