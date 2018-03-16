5cd861725fa150bb6793acd2ab744229cad6363f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-16 10:27:24 -0400

    FIX: CIF [parsing of exotic CIF files Autogenerated CIF files can contain many blocks. Changed in CIFParser has been made so that the first block where lattice parameters are located is used, rather than the first block always. Former-commit-id: bec17028935272ec5d70d3af078c4b8ab4e3c871

--- skued/structure/cif_parser.py
+++ skued/structure/cif_parser.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7495f1540baf59a5ac1ed2c9ef0a35a774bf5c2bd89d015156384fdd4a99e2e0
-size 10313
+oid sha256:2883302a9c4a215f8067b39154c8cae014d0a917cc36d65e7c2307ae97978863
+size 10655
