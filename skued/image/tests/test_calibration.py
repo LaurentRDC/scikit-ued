0c5d39a7cf37af1cfc99158812da50c924554e4d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-16 16:55:28 -0400

    Added calq, powder_calq Former-commit-id: 2776f6dede6bba473cfd1eeb3245f273a64f9714

---
+++ skued/image/tests/test_calibration.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f33db58aa95e6d8511b7b20d98cb270e2d94a7b6e9f18ad5c50e56360d5df95
+size 2247
--- skued/image/tests/test_powder.py
+++ skued/image/tests/test_powder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9ab93ccf5baddafe24dcd3249f1897e99df91eb697cac2e9b6688a891b9e897
-size 7360
+oid sha256:f8c3f55e2999419e0396076ac633dc770849cda9bf7e32ae51efac927410473a
+size 6392
