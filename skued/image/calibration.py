04ee440e2619cdb87ceff73369899a6f674e938c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-18 14:45:19 -0400

    Hide calq until tests Former-commit-id: 05d4a522ef331ac99af673727a9201f01ed8b270 [formerly b516cae9980cb710ae12f297c3647fa8a89982ae] Former-commit-id: 5f19066f1b5ad2c59b8cc92305f05478a63af87b

--- skued/image/calibration.py
+++ skued/image/calibration.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3734eb459ce28f28b34e3211b42041e4d212d333f96aa60df39f220730e50275
-size 4165
+oid sha256:3fc2d2e6e8c6253ba4658e73ddbded2cd2d9bc90eee82257a8fe3378af5e680c
+size 4787
