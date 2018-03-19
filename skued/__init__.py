5270ea9e877e9df8cb4ba1034f3fa703af448c45
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-19 12:57:42 -0400

    Bumped version to 0.5.8 Former-commit-id: d7e72031fc7e54effec977ff7f3acea00289b9e8 [formerly 39dd037ee30dd4ee2adfeeb4b552638899478ced] [formerly ac1a79e71a72c1647be64b8da958a328e2e39399 [formerly a5b696876cbdedfb386910992ec974ab91f1ff01]] Former-commit-id: 6d189a9fe4b7ad8631e70deb15da10957e419b8e [formerly 494061011c26b90bf8001e3e4539cd12f92769a3] Former-commit-id: d18e5f27a3ae303450422be0b61cc9b2273751c0

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe727c959f905db3ab07775e30561992d3813ce4c41c607c829c3fa92a577ff0
+oid sha256:d3ddc15fa71064cfbd76d8799a34f6e20c20abf891d4a5d18276dfdf99bdba0f
 size 1763
