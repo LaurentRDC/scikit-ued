7f48db9110207a2c7698ed0ecfd9b096be4dfc19
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-19 11:18:27 -0400

    repr with dynamic class names Former-commit-id: e2db5706072459459e40f3c4875b9caf623a70f3 [formerly 8b69fddcb6182abb02bc9dba28c6f1ae70130912] Former-commit-id: 80591337c4410b418653ba5e04f49f0d56918c29

--- skued/structure/crystal.py
+++ skued/structure/crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65d4f43952b242aad27e21c7c7222481e2c3d4c537ff9d99ec2f3330d51f8e2e
-size 13713
+oid sha256:fa7764803b908549da3f76f48cf804c0ab717b23381e4568d05fb7dd9d7dc863
+size 13835
