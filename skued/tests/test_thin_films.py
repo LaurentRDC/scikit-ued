0549a6e2a6135c6cbc35a444cfb0e5b7603ddb0e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-20 17:36:38 -0400

    Added thin film optical properties Former-commit-id: 40fe84f2b0789232b5b0f5ada01b76a5bca3fc9e [formerly 1e879aed6dc019f53f43d80bca401ede7090fe2e] Former-commit-id: b838f9f7609cc062292ff5d6a1159a792af48c8c

---
+++ skued/tests/test_thin_films.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c29d3a3016f411d1f48cd13ff16978c5c6c3da4c9090b459d50b405049b9b57a
+size 949
---
+++ skued/thin_films.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78b4f25bbff7cf06cdc36e38cf5d3f9af87b2914ca5f6f4e38b26888122d6c97
+size 2326
