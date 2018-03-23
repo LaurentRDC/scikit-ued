cdf06c109ddaa101759d4045ab844829e351cd66
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-23 09:03:51 -0400

    Added chemical composition property Former-commit-id: b0c40e21f4efbe9b8a77a2d2423a644c1b592687

--- skued/structure/base.py
+++ skued/structure/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7041632b9a9fb623c145fa65e8ea19d67565afb628561f96ebef635016a57ba2
-size 4609
+oid sha256:f684f1600dad37b3e3f5b6824fd042b3b19be220ffe230f6604f8077d948cd56
+size 5034
