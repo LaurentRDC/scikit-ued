3a4dad0d476bab6bb40811cfa7357b63afc84ea4
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-23 09:19:22 -0400

    Added tests for chemical composition Former-commit-id: 3e9c3c5f3a0f4407bcc2399f8ea1f9de9466b505

--- skued/structure/tests/test_base.py
+++ skued/structure/tests/test_base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31e2541e0dc9210385516fc4ab609d0ed4203a6533e9e4459082ebaa16dc526c
-size 2795
+oid sha256:cdca980a4a998a6d150895d9b7b82df095318a14f380b5aa3f4f7768e1341f18
+size 3560
