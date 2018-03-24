d1f87e90c5109963e184bb4020c54e64a610cd7e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-24 13:27:43 -0400

    Added lattice parameters to Lattice nad Crystal reprs Former-commit-id: f858dc2fc79b976d122f4185063aaf53a04b6bb6

--- skued/structure/lattice.py
+++ skued/structure/lattice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f94559a530aac9e61cf4aa7c48b3b546d9c1a1110bc35a97e4398fd1477075b3
-size 12249
+oid sha256:300e794129177f7649ff358493bfdadd11a8692714a596201e3bbfec9c57a755
+size 12289
