dde633e726f2d5f9bb87003ba669d7a5c8caca19
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-03-28 12:37:16 -0400

    DOC: tweaks Former-commit-id: 2aa081fecf61dd1727e127c0cdf02910a8bbd133

--- skued/simulation/structure_factors.py
+++ skued/simulation/structure_factors.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c1d8a07385ec4ef6587b5ce968908e23e9d50d458b3dec69f823a6d703f33a5
-size 5373
+oid sha256:159215e69558dfd73928443229c62432a5f9cc23ea9fb974edb35403f3ceab67
+size 5128
