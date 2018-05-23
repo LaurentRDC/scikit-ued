acca3ad5e4321c41707bf223aeeed3804ed1ecf8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-23 14:20:20 -0400

    FIX: lorentz factor calculation

--- skued/tests/test_eproperties.py
+++ skued/tests/test_eproperties.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d71df4ebd9b8ccf6231f823c8dbee7b2de9da63934b4f833800b53f8d77c3fe
-size 1687
+oid sha256:c3b3b032ada284675d5598058055d5241704257227fc14ea245131233bb686e9
+size 1662
