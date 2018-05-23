FIX: lorentz factor calculation

skued/eproperties.py
@@ -18,8 +18,12 @@ def lorentz(keV):
     Returns
     -------
     out : array_like or float
+
+    References
+    ----------
+    .. Kirkland 2010 Eq. 2.2
     """
-    return 1/np.sqrt(1 + (elementary_charge*keV*1e3)/(2*electron_mass*speed_of_light**2))
+    return 1 + (elementary_charge*keV*1e3)/(electron_mass*speed_of_light**2)
 
 def electron_wavelength(keV):
     """ 
@@ -38,10 +42,16 @@ def electron_wavelength(keV):
     
     Returns
     -------
-    out : float
-        Electron wavelength [Angs]
+    out : array_like or float
+        Electron wavelength [:math:`1/\AA`]
+
+    References
+    ----------
+    .. Kirkland 2010 Eq. 2.5
     """
-    return (Planck/np.sqrt(2*electron_mass*elementary_charge*keV*1e3))*lorentz(keV)*1e10
+    eV = elementary_charge * keV * 1e3
+    wavelength_meters = Planck * speed_of_light / np.sqrt( eV * (2*electron_mass*speed_of_light**2 + eV))
+    return wavelength_meters * 1e10  # wavelength in angstroms
 
 def electron_velocity(keV):
     """
@@ -58,10 +68,17 @@ def electron_velocity(keV):
     
     Returns
     -------
-    out : float
-        Electron velocity [Angs/s]
+    out : array_like or float
+        Electron velocity [:math:`\AA / s`]
+
+    References
+    ----------
+    .. Kirkland 2010 Eq. 2.3
     """
-    return Planck/(electron_mass*electron_wavelength(keV))
+    eV = elementary_charge * keV * 1e3
+    m0c2 = electron_mass * speed_of_light**2
+    v_over_c = np.sqrt( eV * (eV + 2 * m0c2)/(m0c2 + eV) )
+    return (c * v_over_c) * 1e10    # speed in Angstroms
 
 def interaction_parameter(keV):
     """
@@ -74,7 +91,7 @@ def interaction_parameter(keV):
     
     Returns
     -------
-    out : float
+    out : array_like or float
         Interaction parameter [rad/(V*Angs)]
 
     References

skued/tests/test_eproperties.py
@@ -15,11 +15,10 @@ class TestLorentz(unittest.TestCase):
         self.assertTrue(np.allclose(lorentz(kV), np.ones_like(kV)))
     
     def test_range(self):
-        """ Test that lorentz factor is always in the range (0, 1] """
+        """ Test that lorentz factor is always in the range [1, infty) """
         kv = np.linspace(0, 1e6, num = 256)
         factors = lorentz(kv)
-        self.assertTrue(np.all(factors <= 1))
-        self.assertTrue(np.all(factors > 0))
+        self.assertTrue(np.all(np.greater_equal(factors, 1)))
 
 class TestElectronWavelength(unittest.TestCase):
     