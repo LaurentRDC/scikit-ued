f684b5d254123762a0ba169541ee6815d388b0cf
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-23 14:20:40 -0400

    Bump version to 0.5.9 Former-commit-id: c32f41879cbfd3e3b436e1d1ab9d73030d725655

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:697190aa96837e7a7f1abe46f7a66eceda1c1ef1859cb30ff5cecc0ff1e9aaef
+oid sha256:5cdcbe1f122d7228f664a0db6e98f21cf4ec41bd4cd247baf4eeb533bb4b5689
 size 1828
