77cb2dca7329c711fd7c019c7b4ac96f30e0aeb7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-23 14:37:41 -0400

    Added test for str vs repr

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77c1f8d69e6d3f0848d17c68b063c6267e09c9b76c32d5a83ebc3c730f03e45e
-size 6115
+oid sha256:cf97c3f2fafe1d19edac8d9c563d8e311bc2ea99a1d57fc48a917c34736d69f0
+size 6622
