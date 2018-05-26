64969bdbb0995565d25009360856f12de2fb9eca
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-26 13:02:51 -0400

    FIX: mad raising type error on non-float arrays Former-commit-id: 4baaf1bdf9994b78331fd2f41157c7a085defacc

--- skued/time_series/tests/test_robust.py
+++ skued/time_series/tests/test_robust.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80dfdb96c1541ee6fb887f55c553a5b12b03fef3c83f742d6b29f776cb68e0fe
-size 828
+oid sha256:f67430b4f439f8e829312f88eb62402d9dbd4680e8a7e181f11f7a3c79a16870
+size 1180
