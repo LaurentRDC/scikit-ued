2bbfbe027af0c4b2719c9e8e9fe5c0ef6764d12a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-28 10:06:58 -0400

    FIX: removed mutable default arguments Former-commit-id: 33552f0b261cdb415b583189d1222e3fc910268c

--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3c770e5dcefddd754b55213914a3f42db81700712a1dd6d0d8b0db26bedde23
-size 14316
+oid sha256:4077ed624ca1bcae6e0693b45f126819ac0d03943b114b221260932cd67bb62d
+size 14395
