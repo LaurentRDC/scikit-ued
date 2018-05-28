7e16f848cd31bf5f9f1a8c21bcd5277fe0634c40
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-28 11:05:29 -0400

    Removed the pining of conda version in appveyor

--- .appveyor/install-miniconda.ps1
+++ .appveyor/install-miniconda.ps1
@@ -5,12 +5,6 @@
 
 $MINICONDA_URL = "https://repo.continuum.io/miniconda/"
 
-# We pin the version for conda as it's not the most stable package from
-# release to release. Add note here if version is pinned due to a bug upstream.
-if (! $env:CONDA_VERSION) {
-   $env:CONDA_VERSION = "4.3.6"
-}
-
 function DownloadMiniconda ($version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
     $filename = "Miniconda3-" + $version + "-Windows-" + $platform_suffix + ".exe"
@@ -94,8 +88,6 @@ if ($env:CONDA_CHANNELS) {
 }
 
 # Install the build and runtime dependencies of the project.
-conda install -q conda=$env:CONDA_VERSION
-
 if (! $env:CONDA_CHANNEL_PRIORITY) {
    $CONDA_CHANNEL_PRIORITY="false"
 } else {
