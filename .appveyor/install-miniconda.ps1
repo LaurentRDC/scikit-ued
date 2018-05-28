3adacbff7597f47eec006deef92a13f4cdfac4ba
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-28 11:05:29 -0400

    Removed the pining of conda version in appveyor Former-commit-id: 7e16f848cd31bf5f9f1a8c21bcd5277fe0634c40

--- .appveyor/install-miniconda.ps1
+++ .appveyor/install-miniconda.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a056844ed04d45d6a930cfbfcc2359b89cdd7d6a40045cf075c0ab73137a7da2
-size 5727
+oid sha256:cd9e776cc91965bcb81960f5b516fa92995b45fed5fcd8ac7e85e8b97a8d78f4
+size 5469
