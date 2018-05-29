0e4b7dd55460dbf00c4ed90090ec556ecbf1472a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-29 08:50:02 -0400

    cleaned up conf.py Former-commit-id: f076df2b6419eb6ff997ff1ebf4317398f4e2ca8 [formerly eabfd2fa2b00855cbfd6fa0771c3eac3e3307970] Former-commit-id: 854742f1c4677da69e753ed8a70ac5c9e94b0e8c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62acc60b88222f26535938865d122bf2721c1cbc3689b5ac7999f896d835d23f
-size 4697
+oid sha256:2af259b38d140fe8835041c6f1865c03b2fd5da490ca047ecee3418d7aa7e73a
+size 4469
