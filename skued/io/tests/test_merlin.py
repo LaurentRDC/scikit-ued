676a5efd05784960a19c42f38a001e00af9178ea
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-05-31 16:48:40 -0400

    Upgraded filepaths to pathlib Former-commit-id: 67d78bb14d2e0792794c8fc8a0c97c04808aea2c

--- skued/io/tests/test_merlin.py
+++ skued/io/tests/test_merlin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f40dddfcf1e71262e89c4c5605dd659e5d4c6d45098b43720e51ab194f0036
-size 1814
+oid sha256:6f4a06e0f508cffa5513d6c081064d924a473c7364eafa3a61e15fdca5e25676
+size 1804
--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf97c3f2fafe1d19edac8d9c563d8e311bc2ea99a1d57fc48a917c34736d69f0
-size 6622
+oid sha256:34247572d65bd7c5dd2ccae7c404ef80e146fca61956ba94b84d7077301df8f8
+size 6620
