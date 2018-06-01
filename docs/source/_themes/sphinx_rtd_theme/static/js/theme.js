45d33daf1a1c846060e5f6ce197346f19e0dcf61
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-01 10:28:51 -0400

    Updated RTFD theme to latest github version

--- docs/source/_themes/sphinx_rtd_theme/static/js/theme.js
+++ docs/source/_themes/sphinx_rtd_theme/static/js/theme.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b2c5a039d3c0617c95882a3c5d27b04c18d044b759db3eb7898d1b974ebab68
-size 6477
+oid sha256:d384bf07bae1ab1812840dd5c89f63466334d19f3fe9e3ab6d267412add12a7b
+size 4344
