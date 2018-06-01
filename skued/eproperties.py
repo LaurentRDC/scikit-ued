425bb0b4399cced475b37ea9b838e781c63f302a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-01 10:16:32 -0400

    Added potential_map function, documentation and tests Former-commit-id: 85202f77b7f618a4323f6b12c54a59e495a5b7a7 [formerly 7af186534eff452593c133a3585ad82a81b1a775] Former-commit-id: 6ca45910c89ae6d026da9dd8166c9a3dba01197b

--- skued/eproperties.py
+++ skued/eproperties.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4da03f72141b783b2626af9f589865ac3b5365876d985cc7c4a9c48cf98737c7
-size 2561
+oid sha256:0ac8e22106dd0bf22b8feb446ebc78bbaffdb6aa96c9cfa154e0fde4080d1a6d
+size 2568
