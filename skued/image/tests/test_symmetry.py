fb18deba827cc37e220230a62ec2d9df47280800
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 10:54:45 -0400

    FIX: matplotlib import in tests

--- skued/image/tests/test_symmetry.py
+++ skued/image/tests/test_symmetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5b0990f14294eb5363fb6ed4512ae0dc89745c8a394f85375b2e7694e255b54
-size 5622
+oid sha256:f58d35f8fc8d44f86e3c0f062e18493381a0fa406771ea5cdf31c63d3a510024
+size 5560
