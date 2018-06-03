70751b93c959bf824d520c54676b6c3ba772fcb4
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 13:18:21 -0400

    FIX: typo in classifiers Former-commit-id: 3a9ad9a38a8838229dd046a3ad701104a5d257d2 [formerly 0092b949bb0fe199e4aa433240b325d0b21ae4c7] Former-commit-id: 1d6673b1987c64fdbdfb326580358822b96f71ed

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d681fa6bee5c925af46f5a15bff81d60b1209c39d6bca25316d61c293a52a3a2
-size 3375
+oid sha256:2ff4d6f07da5902f21e365c24a16e9476d0f01335d8eff13150aa446e5e9d86f
+size 3386
