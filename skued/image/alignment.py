81fb7822f2e2fa2b4a6cce42babbd20cc0345dc5
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 12:55:40 -0400

    DOC: fixes Former-commit-id: 062b94a7ac6c788fe628f458a2db9bedf70b2e83 [formerly 81fa00217a63c6ba0c4a01f90b039a6634101118] Former-commit-id: aa13283c7a91efccd867545fc9cd6080434dcc91

--- skued/image/alignment.py
+++ skued/image/alignment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b497fd0ca5bf3ce2230f33cbc238820ce29593962838e6a14bf3af2afc26ae
-size 8598
+oid sha256:9becf42953a07863a261a68d372cbb9204eaef2fade61f72b3211b5f7bb61677
+size 8669
