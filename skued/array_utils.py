ae6826bd7c20cae847ee4c972da363a9989c9eac
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 10:52:32 -0400

    DOC: reshuffled tutorials around Former-commit-id: addbf37bbd78358fb55fe0b87f1784676db76338

--- skued/array_utils.py
+++ skued/array_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e077b9d5e9b6b9e2fe684370d571f269231753e2417f618241841f5e6263894
-size 5345
+oid sha256:de1f8679be784fec4b92713df13adfbae5936f75041132cebec020af04059e10
+size 5379
