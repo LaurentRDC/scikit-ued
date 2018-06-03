8125429a726b6331f5f195324fbfb1b081e2e66a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 10:52:32 -0400

    DOC: reshuffled tutorials around Former-commit-id: 18dd0d1493d4ee28420b8a078984d2b4781dfd12 [formerly addbf37bbd78358fb55fe0b87f1784676db76338] Former-commit-id: 99491f37eb9c473ad9d6b97dedf3b64b21dfd4de

--- docs/source/tutorials/simulation.rst
+++ docs/source/tutorials/simulation.rst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8495be5c8a7c80ef156f8d487f6076d62399233e8a4ccfb9eb44521a9c5cfd1
-size 2853
+oid sha256:b2135c46b5348015bcc4f51cc3f9b5bde7fd223e8d45aedf32b9ae3fe8fa5330
+size 2829
--- skued/io/io.py
+++ skued/io/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80e515811e71bd5fe9799029dfecc9db3dc325ace737d25d0fdfa4b1cb98e01f
-size 1983
+oid sha256:d6c3ea688aad98fbc5c23107904a8ca0b8ad364a6d4b745e1ad0acb39cbbf001
+size 1993
