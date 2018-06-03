2204af8d049c412483e4c3f072e3bdf9e05168cd
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-03 10:52:32 -0400

    DOC: reshuffled tutorials around

--- docs/source/tutorials/baseline.rst
+++ docs/source/tutorials/baseline.rst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f6be946c58ccfa01406fb6508b019d088dae85eb59a33db99f030ab65134845
+oid sha256:2e51b238d50ee7bd552ed0b8e55adfe460109a28205ed96cd98d4129bb9db2dd
 size 7129
--- skued/baseline/algorithms.py
+++ skued/baseline/algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e12a0954add7e7cdf53bab4eb41a8d001bb616d646dc21aa5adce6b0d9e9e20
+oid sha256:c303bc835685ddf25dbe465fd42e9c2c277e99cda4e64889737e15f1415daf36
 size 14401
