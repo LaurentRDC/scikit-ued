1c269d99a6c52888dd65a25eedcf9d2ff05d90a2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 09:43:51 -0400

    Suppressed some warnings during testing Former-commit-id: d3d0f760a068d9386df2a00c95c376a3f038a5cb [formerly 016a3752caae1ae118d83f50792086375452ad6a] Former-commit-id: 631bca0456f2a3d1b1df0b0e7e5d5ae73beca12c

--- skued/tests/test_potential_map.py
+++ skued/tests/test_potential_map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfcd2ea17f5d98450abb7f3874095bdf9c34d5f6e2731c884bbabb476a8fe286
-size 1685
+oid sha256:1e8c8144d261df7f14b9298c02f4e731e5c98305976a10898193f4ef29df6d75
+size 1847
