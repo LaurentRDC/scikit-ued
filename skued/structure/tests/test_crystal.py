39baafd095c90cd16940c2001399fb687b458746
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 09:32:49 -0400

    Made test cache a temporary directory Former-commit-id: 280ce4fa7a54f0efe6f9e0f42b49cec10989de15 [formerly bc7d444e0d2733c81878c76b047fd1b3869b40b8] Former-commit-id: 8a747c3ac3695a6e799d5676fbe904263b7030bf

--- skued/structure/tests/test_crystal.py
+++ skued/structure/tests/test_crystal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34247572d65bd7c5dd2ccae7c404ef80e146fca61956ba94b84d7077301df8f8
-size 6620
+oid sha256:5d04bb6f5583ed86cc130c3a3eb0795fa8b29af36d08161d49e5e05cea1a1378
+size 6729
