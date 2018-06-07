6e772d5be018402c21a1de47602ef2b2fef145e1
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 11:03:34 -0400

    Added DM3/DM4 reading capabilities Former-commit-id: fe092c74565d6f48d62ac0d0f54bd4206cc4f1c0

--- skued/__init__.py
+++ skued/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a22693c89513d337e6371f37768952fc6958e5670f3de35aa740fcdf622d4eeb
-size 1924
+oid sha256:cc4ba6c9767e9e51bea9cbc7af425358d1938a2625ad70a43f89753fb6124b86
+size 1932
--- skued/io/tests/test_io.py
+++ skued/io/tests/test_io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4cdfcda023d2c31c3cb5434f685f4b8c50251d6b3faec9c8c65ea2232aa6fae
-size 956
+oid sha256:00b07daea254564469d4eb5694158bce5e501a763dbe32f066928e96dd50d8de
+size 3201
