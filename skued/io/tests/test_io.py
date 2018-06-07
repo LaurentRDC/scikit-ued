427e64b453be514e049f5eefe4907fb57689e614
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 09:43:51 -0400

    Suppressed some warnings during testing Former-commit-id: 66838c122d70795bdef1f7d4dd30b623c2ec2c99 [formerly 26d493720322ad85a6e280f4a681568ea4a1813c] [formerly 631bca0456f2a3d1b1df0b0e7e5d5ae73beca12c [formerly 016a3752caae1ae118d83f50792086375452ad6a]] Former-commit-id: 1d946f14b76218dd6840427e8e46d1e2dd1d7d5e [formerly b351fdaf8f877818186dcaafe769d800e1a41e22] Former-commit-id: 3bfdccfbf0f085befde8a7af27944c6b9ce83a75

--- skued/io/tests/test_io.py
+++ skued/io/tests/test_io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ae1d141556234dd9e153d222d80041d367ce414f4e83c2a3a09cba62275f33
-size 838
+oid sha256:c4cdfcda023d2c31c3cb5434f685f4b8c50251d6b3faec9c8c65ea2232aa6fae
+size 956
