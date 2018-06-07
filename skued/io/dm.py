166f86dcd6ee6dc49760fb5b52a89671048fc348
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 11:52:03 -0400

    DOC: DOC: updated tutorials Former-commit-id: 246bacefc6166bb48d35390fd3cc8643029ae5d4 [formerly 15b2ca8a43b4b9a360bd8d09285d7b22da07acd8] Former-commit-id: 8c528dfb0ca6e264af71f3c6865c61d9a237dc74

--- skued/io/dm.py
+++ skued/io/dm.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e51e24010f500dd97afca4cdf52d5e7b5b53f63c393d0b1e06d08d33b14e60a9
-size 22756
+oid sha256:b6dc93373f6463cbb948f6a4e23bdac262e30cb4f228b139fc758d30bc0b2458
+size 22849
