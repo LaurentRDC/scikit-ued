46e44aaff45ad86d037d7806f1d64558c9f5da2d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-07 09:32:49 -0400

    Made test cache a temporary directory

--- skued/structure/tests/test_parsers.py
+++ skued/structure/tests/test_parsers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e88d9544c435ff61741429bdc475f34ed5ffc59dc1bbb163e0e1313631f715d
-size 4031
+oid sha256:a8e517e2afcf705b4734f99747c8da79edeafbe19c12031aaaa193c873a5675b
+size 4183
