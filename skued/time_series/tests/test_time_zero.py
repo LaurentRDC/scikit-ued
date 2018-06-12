3536d10cf1c47b7ef1f7248f65a0830e3e94577c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-12 13:34:35 -0400

    Added test for register_time_shift Former-commit-id: 15cf10a235996f7c1b9c874988b0e8131635afaf

--- skued/time_series/tests/test_time_zero.py
+++ skued/time_series/tests/test_time_zero.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e277611a34d842aaf3e0e5e4c8e85ba7c3164e81115c1e5d49797046a35d4a9
-size 3488
+oid sha256:deee9ff432e200070101323fe6fc6daf57658291559415a495cfb5a130e6c8c7
+size 4082
