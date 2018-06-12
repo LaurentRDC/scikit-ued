8ae89dea1be61b335a4c56ff43e7f87caad669d2
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-12 13:31:35 -0400

    FIX: improper normalization in time_shift function. Cross-correlation measurements of time-shifts was not properly normalized. Took the opportunity to deprecate time_shift and time_shifts in favour of two new functions with correct normalization, register_time_shift(s). Former-commit-id: c7537e43b792eca5ca85b5aa2b90e3b1d53bd60e

--- skued/utils.py
+++ skued/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:317447fdac5c76badd3ed28367200116773afe85da71c1b3ed921f134dd2a3b5
-size 1109
+oid sha256:e81a25e1f15d490af7a4720b5753aeeb396117fa610e352c73eef401578054af
+size 1043
