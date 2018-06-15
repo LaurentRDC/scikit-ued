c499a5476bdd128c8e56c5b0daa077cd48d76e2e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-15 15:19:51 -0400

    Removed binary dependencies This is not required since we are now available on conda-forge Former-commit-id: 67ea1f7595a4286f02094f3a70a65152c6d88bbc [formerly a01586168a1701c961f2b2ba1e6c30a6721f4ae3] Former-commit-id: 66c9898c31489a68aa7cf66d2983e531ba739233

---
+++ skued/image/correlation.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4196cb97bdce0304cdd94d6b700bb073c38e16bb45230511fce37604f03b5dba
+size 8229
---
+++ skued/image/tests/test_metrics.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf3303b0dc4ba660f6c6a63d0f1ddaccf04625e763221bbdb71f4120345ca96c
+size 5977
