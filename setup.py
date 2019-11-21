3e1a8f7cd5148d96336c2fb546be1f70c2355039
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-11-21 15:58:45 -0500

    Added explicit support for Python 3.8

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ if __name__ == "__main__":
             "Programming Language :: Python",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
             "Topic :: Scientific/Engineering",
             "Topic :: Scientific/Engineering :: Physics",
         ],
