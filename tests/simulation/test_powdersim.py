0e126fa35732f12c8e234da59319e448db8c919b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-04-10 11:24:07 -0400

    Added alignment test for powdersim

--- tests/simulation/test_powdersim.py
+++ tests/simulation/test_powdersim.py
@@ -9,15 +9,26 @@ from skued import powdersim
 
 
 class TestPowdersim(unittest.TestCase):
-    def setUp(self):
-        self.q = np.linspace(2, 10, 200)
-        self.crystal = Crystal.from_database("C")
 
     def test_return_shape(self):
         """ Test that the return shape of powdersim() is as expected """
-        pattern = powdersim(self.crystal, self.q)
-        self.assertSequenceEqual(pattern.shape, self.q.shape)
+        q = np.linspace(2, 10, 200)
+        pattern = powdersim(Crystal.from_database("C"), q)
+        self.assertSequenceEqual(pattern.shape, q.shape)
+    
+    def test_peak_alignment(self):
+        """ Test that the diffraction peaks align with what is expected. """
+        crystal = Crystal.from_database("C")
 
+        for reflection in [(0,1,1), (1,2,0), (-1, 2, 0)]:
+            with self.subTest(f'Reflection {reflection}'):
+                qknown = np.linalg.norm(crystal.scattering_vector((0,1,1)))
+
+                # Range of scattering vectors is tightly centered around a particular reflection
+                # So that the maximum of the diffraction pattern MUST be at reflection (010)
+                q = np.linspace(qknown-0.1, qknown + 0.1, 256)
+                pattern = powdersim(Crystal.from_database("C"), q)
+                self.assertAlmostEqual(q[np.argmax(pattern)], qknown, delta=q[1] - q[0])
 
 if __name__ == "__main__":
     unittest.main()
