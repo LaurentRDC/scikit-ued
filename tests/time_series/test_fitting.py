8d76f692aed9aa8f623c5fef7d022054611496d7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-07-08 10:22:53 -0400

    Added tests for with_irf

--- tests/time_series/test_fitting.py
+++ tests/time_series/test_fitting.py
@@ -3,7 +3,7 @@ from random import random, seed
 
 import numpy as np
 
-from skued import biexponential, exponential
+from skued import biexponential, exponential, with_irf
 
 seed(23)
 
@@ -126,6 +126,45 @@ class TestBiExponentialDecay(unittest.TestCase):
         )
 
         self.assertTrue(np.allclose(exp, biexp))
+
+
+class TestWithIrf(unittest.TestCase):
+    def test_trivial_constant_spacing(self):
+        """ Test with_irf with a trivial IRF, with constant spacing """
+        params = (0, 1, 3)
+        times = np.linspace(-5, 15, 256)
+        data = exponential(times, *params)
+
+        @with_irf(0.00001)  # vanishingly small irf
+        def exponential_with_irf(time, *args, **kwargs):
+            return exponential(time, *args, **kwargs)
+
+        conv = exponential_with_irf(times, *params)
+
+        self.assertTrue(np.allclose(data, conv))
+
+    def test_trivial_nonconstant_spacing(self):
+        """ Test with_irf with a trivial IRF, with non-constant spacing """
+        # Note that the spacing of the steps is important for this test
+        # If the array `times` is of even length, then the convolution will result
+        # in one time-step shift
+        params = (0, 1, 3)
+        times = np.concatenate(
+            (
+                np.arange(-10, -2, step=1),
+                np.arange(-2, 2, step=0.04),
+                np.arange(2, 10, step=1),
+            )
+        )
+        data = exponential(times, *params)
+
+        @with_irf(0.00001)  # vanishingly small irf
+        def exponential_with_irf(time, *args, **kwargs):
+            return exponential(time, *args, **kwargs)
+
+        conv = exponential_with_irf(times, *params)
+
+        self.assertTrue(np.allclose(data, conv))
 
 
 if __name__ == "__main__":
