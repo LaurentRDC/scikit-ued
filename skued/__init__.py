0e6d83252dad6ae04dbe031a83e5939a6e3b86ce
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-01-22 09:54:01 -0500

    version bump

--- skued/__init__.py
+++ skued/__init__.py
@@ -2,7 +2,7 @@
 __author__ = "Laurent P. René de Cotret"
 __email__ = "laurent.renedecotret@mail.mcgill.ca"
 __license__ = "GPLv3"
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 
 from .affine import (
     affine_map,
